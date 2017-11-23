281dd65430d7fd7dc22d24f931a39925b8fd8695
eFEX board <efex@efex-vm2.cern.ch>
2017-11-23 14:09:34 +0100

    Add virtual machine config files

---
+++ vm-config/setup_vm.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+yum install htop emacs git eos-fuse doxygen
+
+mkfs.ex4 /dev/vdb
+mkdir /mnt/vd
+chmod a+xrw /mnt/vd
+mount /dev/vdb /mnt/v
+
+addusercern efex
+# making efex home
+mkdir /home/efex
+chmod a+rxw /home/efex
+
+# copying git config file
+cp gitconfig /home/efex/.gitconfig
+
+cp -r ssh/ /home/efex/.ssh
+cp bash_profile /home/efex/.bash_profile
+cp -r bashrc /home/efex/.bashrc
+# change home directory in /etc/passwd
+
+# go to vivado installation dir
+./xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config ../install_config.txt
+
+# copy eos config files from lxplus
+cp ./eos_config/* /etc/sysconfig/
+
+# copy krb5 config file
+cp ./krb5.conf /etc
+
+systemctl enable eosd.service
+systemctl start eosd.service
+eosfusebind krb5
+
+dd if=/dev/zero of=/swapfile bs=1024 count=16777216
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile

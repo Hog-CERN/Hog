ff163dc9a3aae6a1dd965c58707716a7559eaa5e
efex <e.fex@cern.ch>
2017-11-30 12:28:14 +0100

    work on hooks, still in progress

--- hooks.py
+++ hooks.py
@@ -16,27 +16,46 @@ class hooks:
         data = json.loads(web.data())
         sb=data['object_attributes']['source_branch']
         tb=data['object_attributes']['target_branch']
+        n=data['object_attributes']['iid']
+        state=data['object_attributes']['state']
+        wip=data['object_attributes']['work_in_progress']        
+        status=data['object_attributes']['merge_status']
+        description=data['object_attributes']['description']
+        title=data['object_attributes']['title']
 
         print
         print 'DATA RECEIVED:'
         print "--------------------------------"
-        pprint(data)
+        #pprint(data)
         print
-        print "--------------------------------"
-        print "Merge commit SHA:", data['object_attributes']['merge_commit_sha']
-        print "Merge request N: ", data['object_attributes']['iid']
+        print "Merge request N: ", n
         print "Source branch:   ", sb 
         print "Target branch:   ", tb 
-        print "State:           ", data['object_attributes']['state']
-        print "Work in progress:", data['object_attributes']['work_in_progress']
-        print "Title:           ", data['object_attributes']['title']
-        print "Description:     ", data['object_attributes']['description']
-        print "Merge status:    ", data['object_attributes']['merge_status']
+        print "State:           ", state 
+        print "Work in progress:", wip 
+        print "Title:           ", title 
+        print "Description:     ", description 
+        print "Merge status:    ", status 
+        print "--------------------------------"
 
-        cmd = "kinit -kt /home/efex/efex.keytab efex; /usr/bin/eosfusebind krb5; /bin/bash /home/efex/AutomationScripts/AutoLaunchRun.sh /home/efex/eFEXFirmware {0} {1} /mnt/vd/eFEX-revision /eos/user/e/efex/www/revision".format(sb,tb)
-        print "Executing {0}".format(cmd)
-        os.system(cmd + "&")
+        # For pre merging test use
+        #if status == 'can_be_merged' and state == 'opened' and not wip:
+        if status == 'can_be_merged' and tb= 'master' and state == 'merged' and not wip:
+            print "*******************************************"
+            print "Launching run for merge request {0}".format(n)
+            print "From: {0}   To: {1}".format(sb,tb)
+            print "Title: ".format(title)
+            print 
+            print "Description: "
+            print description
+            print
+            print "*******************************************"
+            cmd = "kinit -kt /home/efex/efex.keytab efex; /usr/bin/eosfusebind krb5; /bin/bash /home/efex/AutomationScripts/AutoLaunchRun.sh /home/efex/eFEXFirmware {0} {1} /mnt/vd/eFEX-revision /eos/user/e/efex/www/revision".format(sb,tb)
+            print "Executing {0}".format(cmd)
+            #        os.system(cmd + "&")
+
         return 'OK'
+
         
     def AddSynth(self, from_branch, to_branch):
         print "Adding to queue: {0} {1}".format(from_branch, to_branch)

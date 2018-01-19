974d0b3ebfc0f4bbf9e4a21f8f079bef01d75fc7
Francesco Gonnella <francesco.gonnella@cern.ch>
2018-01-19 13:25:58 +0000

    Add new python program

---
+++ AutoWorkflowSystem.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import subprocess
+import shutil
+
+def Run(command, path='.', verbose=False):
+    if verbose:
+        print "Running: '", command, "' From: '", path, "'"
+    cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, cwd=path)
+    return [x.rstrip() for x in list(cmd.stdout)]
+
+#print Run("git log --format=%h -1 -- $(awk '!/^ *#/ && NF {print $1}' ./list/*) .")[0]
+#print Run("ls", "./list")
+
+def SendMail(txt, subject, address):
+    Run("echo \"{0}\" | mail -s \"{1}\" {2}".format(txt, subject,address))
+
+print Run("ls")

b75f876ca36b4e46011fdbad46d1fa0088447cdd
Francesco Gonnella <francesco.gonnella@cern.ch>
2018-03-29 15:30:48 +0100

    Make setup-vum efex independent

--- awe/vm-config/setup_vm.sh
+++ awe/vm-config/setup_vm.sh
@@ -1,77 +1,120 @@
 #!/bin/bash
-if [ -v VIVADO_DIR]; then
-    echo "[VM Setup] Vivado installation direcotry is set to $VIVADO_DIR"
+if [ -v AWE_PATH]; then
+    echo "[awe-VM Setup] awe repository path set to $AWE_PATH"
 else
-    VIVADO_DIR=/afs/cern.ch/work/f/fgonnell/Xilinx_Vivado_SDK_2017.3_1005_1/
-    echo "[VM Setup] Automatically setting Vivado installation direcotry to $VIVADO_DIR"
+    echo "[awe-VM Setup] variable AWE_PATH should be set to the root of the repository containing Hog/awe"
+    exit 1
 fi
 
-echo "[VM Setup] Installing useful packages..."
+if [ -v AWE_VIVADO_DIR]; then
+    echo "[awe-VM Setup] Vivado installation direcotry is set to $AWE_VIVADO_DIR"
+else
+    VIVADO_DIR=/afs/cern.ch/work/f/fgonnell/Xilinx_Vivado_SDK_2017.3_1005_1/
+    echo "[awe-VM Setup] ERROR: variable AWE_VIVADO_DIR should be set and point to a valid Xilinx Vivado SDK installation directory."
+    exit 1
+fi
+
+if [ -v AWE_USERNAME]; then
+    echo "[awe-VM Setup] The user awe will use is: $AWE_USERNAME"
+else
+    echo "[awe-VM Setup] ERROR: variable AWE_USERNAME should be set with a valid CERN user name"
+    exit 2
+fi
+
+if [ -v AWE_USERGROUP]; then
+    echo "[awe-VM Setup] The user group awe will use is: $AWE_USERGROUP"
+else
+    echo "[awe-VM Setup] ERROR: variable AWE_USERGROUP should be set with a valid CERN user group"
+    exit 3
+fi
+
+if [ -v AWE_PRIVATETOKEN]; then
+    echo "[awe-VM Setup] The private token for gitlab acces is set to : $AWE_PRIVATETOKEN"
+else
+    echo "[awe-VM Setup] ERROR: variable AWE_PRIVATETOKEN should be set with a valid gitlab private token"
+    exit 3
+fi
+
+if [ -z "$AWE_WEB_PATH" ] || [ -z "$AWE_REVISION_PATH" ] || [ -z "$AWE_PATH" ]; then
+  echo "[awe-VM Setup] ERROR: variable AWE_WEB_PATH, AWE_REVISION_PATH should be set with a valid path"
+  exit 1
+fi
+
+
+echo [awe-VM Setup] Adding $AWE_USERNAME user...
+addusercern $AWE_USERNAME
+echo [awe-VM Setup] Making $AWE_USERNAME home...
+mkdir /home/$AWE_USERNAME
+chmod a+rxw /home/$AWE_USERNAME
+chown $AWE_USERNAME:$AWE_USERGROUP /home/$AWE_USERNAME
+
+echo "[awe-VM Setup] Installing useful packages..."
 yum install htop emacs git eos-fuse doxygen python-webpy screen
 
-echo "[VM Setup] Installing wandisco repository..."
+echo "[awe-VM Setup] Installing wandisco repository..."
 yum install http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
-echo "[VM Setup] Updating to recent version of git from wandisco..."
+echo "[awe-VM Setup] Updating to recent version of git from wandisco..."
 yum --disablerepo=base,updates  update git
 
-echo "[VM Setup] Installing uhal from ipbus..."
+echo "[awe-VM Setup] Installing uhal from ipbus..."
 curl http://ipbus.web.cern.ch/ipbus/doc/user/html/_downloads/ipbus-sw.centos7.x86_64.repo > ipbus-sw.repo
 cp ipbus-sw.repo /etc/yum.repos.d/
 yum groupinstall uhal
 
-echo "[VM Setup] Config files into efex's home"
-cp gitconfig /home/efex/.gitconfig
-chown efex:zp /home/efex/.gitconfig
-cp -r ssh/ /home/efex/.ssh
-chown -R efex:zp /home/efex/.ssh
-chmod 600 /home/efex/.ssh/*
-cp bash_profile /home/efex/.bash_profile
-chown efex:zp /home/efex/.bash_profile
-cp -r bashrc /home/efex/.bashrc
-chown efex:zp /home/efex/.bashrc
+echo "[awe-VM Setup] Config files into $AWE_USERNAME's home"
+cp gitconfig /home/$AWE_USERNAME/.gitconfig
+chown $AWE_USERNAME:$AWE_USERGROUP /home/$AWE_USERNAME/.gitconfig
+cp -r ssh/ /home/$AWE_USERNAME/.ssh
+chown -R $AWE_USERNAME:$AWE_USERGROUP /home/$AWE_USERNAME/.ssh
+chmod 600 /home/$AWE_USERNAME/.ssh/*
+cp bash_profile /home/$AWE_USERNAME/.bash_profile
+chown $AWE_USERNAME:$AWE_USERGROUP /home/$AWE_USERNAME/.bash_profile
+cp -r bashrc /home/$AWE_USERNAME/.bashrc
+chown $AWE_USERNAME:$AWE_USERGROUP /home/$AWE_USERNAME/.bashrc
 
-echo "[VM Setup] ***************************** "
-echo "[VM Setup] ACTION NEEDED: Please change home directory of efex user to /home/efex in /etc/passwd"
-echo "[VM Setup] ***************************** "
+echo "[awe-VM Setup] ***************************** "
+echo "[awe-VM Setup] ACTION NEEDED: Please change home directory of $AWE_USERNAME user to /home/$AWE_USERNAME in /etc/passwd"
+echo "[awe-VM Setup] ***************************** "
 
-echo "[VM Setup] Copying eos config files..."
+echo "[awe-VM Setup] Copying eos config files..."
 cp ./eos_config/* /etc/sysconfig/
 
-echo "[VM Setup] Copying Kerberos config file..."
+echo "[awe-VM Setup] Copying Kerberos config file..."
 cp ./krb5.conf /etc
 
-echo "[VM Setup] Activating EOS service..."
+echo "[awe-VM Setup] Activating EOS service..."
 systemctl enable eosd.service
 systemctl start eosd.service
 eosfusebind krb5
 
-echo "[VM Setup] Configuring firewall..."
+echo "[awe-VM Setup] Configuring firewall..."
 firewall-cmd --zone=public --add-port=8000/tcp --permanent
 firewall-cmd --reload
 
-echo "[VM Setup] Installing efex AWS service..."
-cp efex-aws.service /etc/systemd/system
-systemctl enable efex-aws.service
+echo "[awe-VM Setup] Installing awe service..."
+envsubst < awe.service > /etc/systemd/system/awe.service
+envsubst < awe.conf > /etc/awe.conf
+systemctl enable awe.service
 
-echo "[VM Setup] Creating swap file, this might take a while..."
+echo "[awe-VM Setup] Creating swap file, this might take a while..."
 dd if=/dev/zero of=/swapfile bs=1024 count=16777216
 chmod 600 /swapfile
 mkswap /swapfile
 swapon /swapfile
 
 if [ -e /dev/vdb ]; then
-    echo "[VM Setup] Formatting and mounting /dev/vdb..."
+    echo "[awe-VM Setup] Formatting and mounting /dev/vdb..."
     mkfs.ex4 /dev/vdb
     mkdir /mnt/vd
     chmod a+xrw /mnt/vd
     mount /dev/vdb /mnt/vd
 else
-    echo "[VM Setup] WARINING /dev/vdb not found"
+    echo "[awe-VM Setup] WARINING /dev/vdb not found"
 fi
 
-if [[ -x "$VIVADO_DIR/xsetup" ]]; then
-    echo "[VM Setup] Installing Vivado, this might take more than a while..."
-    $VIVADO_DIR/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config ./install_config.txt
+if [[ -x "$AWE_VIVADO_DIR/xsetup" ]]; then
+    echo "[awe-VM Setup] Installing Vivado, this might take more than a while..."
+    $AWE_VIVADO_DIR/xsetup --agree XilinxEULA,3rdPartyEULA,WebTalkTerms --batch Install --config ./install_config.txt
 else
-    echo "[VM Setup] Vivado setup not found in $VIVADO_DIR..."
+    echo "[awe-VM Setup] Vivado setup not found in $VIVADO_DIR..."
 fi

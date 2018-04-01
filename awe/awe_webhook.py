2facf2d12d3b43296b215dc178b25221f7790261
Francesco Gonnella <francesco.gonnella@cern.ch>
2018-04-01 15:36:51 +0200

    Add debug feature in aws_webhook

--- awe/awe_webhook.py
+++ awe/awe_webhook.py
@@ -18,88 +18,97 @@ class hooks:
         data = json.loads(web.data())
         if self.Verbose:
             pprint(data)
-        sb=data['object_attributes']['source_branch']
-        tb=data['object_attributes']['target_branch']
-        state=data['object_attributes']['state']
-        wip=data['object_attributes']['work_in_progress']        
-        description=data['object_attributes']['description']
-        title=data['object_attributes']['title']
-        n=data['object_attributes']['iid']
-        url=data['object_attributes']['url']
-        last_commit_author=data['object_attributes']['last_commit']['author']['name']
-        action=data['object_attributes']['action']
-        print "[awe_webhook] --------------------------------"
-        print "[awe_webhook] MERGE REQUEST RECEIVED:"
-        print "[awe_webhook] --------------------------------"
-        print "[awe_webhook] Merge request N:     ", n
-        print "[awe_webhook] Source branch:       ", sb 
-        print "[awe_webhook] Target branch:       ", tb 
-        print "[awe_webhook] State:               ", state 
-        print "[awe_webhook] Work in progress:    ", wip 
-        print "[awe_webhook] Title:               ", title 
-        print "[awe_webhook] Description:         ", description 
-        print "[awe_webhook] Last commit author:  ", last_commit_author 
-        print "[awe_webhook] Action:              ", action
-        time.sleep(1)
-        u = requests.get(url, headers=head)
-        time.sleep(1)
-        r = requests.get("https://gitlab.cern.ch/api/v4/projects/{}/merge_requests/{}".format(RepoAddress,n), headers=head)
-        data_web=json.loads(r.text)
-        status=data_web['merge_status']
-        print "[awe_webhook] Merge status:        ", status 
-        print "[awe_webhook] --------------------------------"
-        sys.stdout.flush()
-        if self.Verbose:
-            pprint(data_web)
-        if status == 'can_be_merged' and tb == 'master' and state == 'opened' and last_commit_author != USERNAME and action != 'approved' and not wip and not 'TEST_MERGE' in description:
-            if 'DRYRUN' in description:
-                DryRun=True
-                print "[awe_webhook] This is a DRY RUN"
-            else:
-                DryRun=False
-
-            if 'NO_TIME' in description:
-                NoTime=1
-                print "[awe_webhook] Time and date will not be added to firmware registers"
-            else:
-                NoTime=0
-
-            if 'MINOR_VERSION' in title:
-                VersionLevel = 1
-                print "[awe_webhook] This is a minor version release candidate x.y.z will become x.(y+1).0 ..."                
-            elif 'MAJOR_VERSION' in title:
-                VersionLevel = 2
-                print "[awe_webhook] This is a major version release candidate x.y.z will become (x+1).0.0 ..."                
-            else:
-                VersionLevel = 0
-
-            thread = Thread(target = StartWorkflow, args = (sb,tb,n, VersionLevel, DryRun, NoTime))
-            thread.start()
-
-
-        elif (tb == 'master' and state == 'merged' and not wip) or 'TEST_MERGE' in description:
-            print "[awe_webhook] Merge request was merged. Tagging new official version..."
-            sys.stdout.flush()
-            f_name = AweFile.format(n)
-            print "[awe_webhook] Open awe file {}...".format(f_name)
-            f = open (f_name, 'r')
-            Run = pickle.load(f)
-            f.close()
-            old_tag = Run.Ver.Tag()
-            new_tag = Run.Ver.Tag(True)
-            print "[awe_webhook] New tag is {}".format(new_tag)
-            sys.stdout.flush()
-            tag_msg = ""
-            tag_note = "##Note:\n this is the release note  \n in markup *format*"
-            ret=  awe.NewTag(new_tag, old_tag, Run.TagMsg(), Run.TagNote())
-            if ret == 201:
-                print "[awe_webhook] New tag created successfully"
-            else:
-                print "[awe_webhook] WARNING: error creating new tag ({})".format(ret)
-            Run.MoveFileOfficial()
-            print "[awe_webhook] All done."
-            sys.stdout.flush()
-        return 'OK'
+        try:
+	    sb=data['object_attributes']['source_branch']
+	    tb=data['object_attributes']['target_branch']
+	    state=data['object_attributes']['state']
+	    wip=data['object_attributes']['work_in_progress']        
+	    description=data['object_attributes']['description']
+	    title=data['object_attributes']['title']
+	    n=data['object_attributes']['iid']
+	    url=data['object_attributes']['url']
+	    last_commit_author=data['object_attributes']['last_commit']['author']['name']
+	    action=data['object_attributes']['action']
+	    print "[awe_webhook] --------------------------------"
+	    print "[awe_webhook] MERGE REQUEST RECEIVED:"
+	    print "[awe_webhook] --------------------------------"
+	    print "[awe_webhook] Merge request N:     ", n
+	    print "[awe_webhook] Source branch:       ", sb 
+	    print "[awe_webhook] Target branch:       ", tb 
+	    print "[awe_webhook] State:               ", state 
+	    print "[awe_webhook] Work in progress:    ", wip 
+	    print "[awe_webhook] Title:               ", title 
+	    print "[awe_webhook] Description:         ", description 
+	    print "[awe_webhook] Last commit author:  ", last_commit_author 
+	    print "[awe_webhook] Action:              ", action
+	    time.sleep(1)
+	    u = requests.get(url, headers=head)
+	    time.sleep(1)
+	    r = requests.get("https://gitlab.cern.ch/api/v4/projects/{}/merge_requests/{}".format(RepoAddress,n), headers=head)
+	    data_web=json.loads(r.text)
+	    status=data_web['merge_status']
+	    print "[awe_webhook] Merge status:        ", status 
+	    print "[awe_webhook] --------------------------------"
+	    sys.stdout.flush()
+	    if self.Verbose:
+	        pprint(data_web)
+	    if status == 'can_be_merged' and tb == 'master' and state == 'opened' and last_commit_author != USERNAME and action != 'approved' and not wip and not 'TEST_MERGE' in description:
+	        if 'DRYRUN' in description:
+	            DryRun=True
+	            print "[awe_webhook] This is a DRY RUN"
+	        else:
+	            DryRun=False
+	    
+	        if 'NO_TIME' in description:
+	            NoTime=1
+	            print "[awe_webhook] Time and date will not be added to firmware registers"
+	        else:
+	            NoTime=0
+	    
+	        if 'MINOR_VERSION' in title:
+	            VersionLevel = 1
+	            print "[awe_webhook] This is a minor version release candidate x.y.z will become x.(y+1).0 ..."                
+	        elif 'MAJOR_VERSION' in title:
+	            VersionLevel = 2
+	            print "[awe_webhook] This is a major version release candidate x.y.z will become (x+1).0.0 ..."                
+	        else:
+	            VersionLevel = 0
+	    
+	        thread = Thread(target = StartWorkflow, args = (sb,tb,n, VersionLevel, DryRun, NoTime))
+	        thread.start()
+	    
+	    
+	    elif (tb == 'master' and state == 'merged' and not wip) or 'TEST_MERGE' in description:
+	        print "[awe_webhook] Merge request was merged. Tagging new official version..."
+	        sys.stdout.flush()
+	        f_name = AweFile.format(n)
+	        print "[awe_webhook] Open awe file {}...".format(f_name)
+	        f = open (f_name, 'r')
+	        Run = pickle.load(f)
+	        f.close()
+	        old_tag = Run.Ver.Tag()
+	        new_tag = Run.Ver.Tag(True)
+	        print "[awe_webhook] New tag is {}".format(new_tag)
+	        sys.stdout.flush()
+	        tag_msg = ""
+	        tag_note = "##Note:\n this is the release note  \n in markup *format*"
+	        ret=  awe.NewTag(new_tag, old_tag, Run.TagMsg(), Run.TagNote())
+	        if ret == 201:
+	            print "[awe_webhook] New tag created successfully"
+	        else:
+	            print "[awe_webhook] WARNING: error creating new tag ({})".format(ret)
+	        Run.MoveFileOfficial()
+	        print "[awe_webhook] All done."
+	        sys.stdout.flush()
+            return 'OK'
+        except KeyError as k:
+            print "[awe_webhook] ERROR: key {} not found in data from Gitlab".format(k)
+            pprint(data)
+            return 'ERROR'
+        except:
+            print "[awe_webhook] ERROR: unexpected error"
+            pprint(data)
+            return 'ERROR'
 
 def StartWorkflow(sb,tb,n,v_level=0,DryRun=False,NoTime=0):
     print "[awe_webhook] *******************************************"

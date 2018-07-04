0dddb9f403ae51f79a0602c4b3a4e23e3ab592fa
Francesco Gonnella <francesco.gonnella@cern.ch>
2018-07-04 20:56:06 +0100

    Real debug

--- Tcl/post-bitstream.tcl
+++ Tcl/post-bitstream.tcl
@@ -6,8 +6,8 @@ source $tcl_path/hog.tcl
 set proj_file [get_property parent.project_path [current_project]]
 set proj_dir [file normalize [file dirname $proj_file]]
 set proj_name [file rootname [file tail $proj_file]]
-set bit_file [file normalize "$old_path/$proj_name.bit"]
-set bin_file [file normalize "$old_path/$proj_name.bin"]
+set bit_file [file normalize "$old_path/top_$proj_name.bit"]
+set bin_file [file normalize "$old_path/top_$proj_name.bin"]
 
 # Go to repository path
 cd $tcl_path/../../ 
@@ -20,10 +20,10 @@ set dst_bin [file normalize "$proj_dir/$proj_name\-$describe.bin"]
 
 if [file exists $bit_file] {
     Info $NAME 1 "Copying bit file $bit_file into $dst_bit"
-    file copy $bit_file $dst
+    file copy $bit_file $dst_bit
     if [file exists $bin_file] {
 	Info $NAME 2 "Copying bin file $bin_file into $dst_bin"
-	file copy $bin_file $dst
+	file copy $bin_file $dst_bin
     }
 } else {
    Warn $NAME 3 "Bit file $bit_file not found"

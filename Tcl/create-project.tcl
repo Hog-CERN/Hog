541fe43fb1e537d408e1df92f2ecb2d3e15fc0d9
Francesco Gonnella <francesco.gonnella@cern.ch>
2018-07-10 10:06:22 +0300

    Change report names

--- Tcl/create-project.tcl
+++ Tcl/create-project.tcl
@@ -128,14 +128,6 @@ if {[string equal [get_property -quiet report_strategy $obj] ""]} {
     if { [llength $reports ] > 0 } {
 	delete_report_config [get_report_configs -of_objects $obj]
     }
-    # Create 'synthesis_synth_report_utilization_0' report (if not found)
-#    if { [ string equal [get_report_configs -of_objects [get_runs synth_1] synth_1_synth_report_utilization_0] "" ] } {
-#	create_report_config -report_name synth_1_synth_report_utilization_0 -report_type report_utilization:1.0 -steps synth_design -runs synth_1
-#    }
-#    set obj [get_report_configs -of_objects [get_runs synth_1] synth_1_synth_report_utilization_0]
-#    if { $obj != "" } {
-#		Info CreateProject 1 "Report post synthesis utilization created successfully"	
-#    }
 }
 
 
@@ -183,20 +175,20 @@ if {[string equal [get_property -quiet report_strategy $obj] ""]} {
 	delete_report_config [get_report_configs -of_objects $obj]
     }
 
-    # Create 'impl_1_route_report_timing_summary_0' report (if not found)
-    if { [ string equal [get_report_configs -of_objects [get_runs impl_1] impl_1_route_report_timing_summary_0] "" ] } {
-	create_report_config -report_name impl_1_route_report_timing_summary_0 -report_type report_timing_summary:1.0 -steps route_design -runs impl_1
+    # Create 'impl_1_route_report_timing_summary' report (if not found)
+    if { [ string equal [get_report_configs -of_objects [get_runs impl_1] $DESIGN\_impl_1_route_report_timing_summary] "" ] } {
+	create_report_config -report_name $DESIGN\_impl_1_route_report_timing_summary -report_type report_timing_summary:1.0 -steps route_design -runs impl_1
     }
-    set obj [get_report_configs -of_objects [get_runs impl_1] impl_1_route_report_timing_summary_0]
+    set obj [get_report_configs -of_objects [get_runs impl_1] $DESIGN\_impl_1_route_report_timing_summary]
     if { $obj != "" } {
 	Info CreateProject 1 "Report timing created successfully"	
     }
 
-    # Create 'impl_1_route_report_utilization_0' report (if not found)
-    if { [ string equal [get_report_configs -of_objects [get_runs impl_1] impl_1_route_report_utilization_0] "" ] } {
-	create_report_config -report_name impl_1_route_report_utilization_0 -report_type report_utilization:1.0 -steps route_design -runs impl_1
+    # Create 'impl_1_route_report_utilization' report (if not found)
+    if { [ string equal [get_report_configs -of_objects [get_runs impl_1] $DESIGN\_impl_1_route_report_utilization] "" ] } {
+	create_report_config -report_name $DESIGN\_impl_1_route_report_utilization -report_type report_utilization:1.0 -steps route_design -runs impl_1
     }
-    set obj [get_report_configs -of_objects [get_runs impl_1] impl_1_route_report_utilization_0]
+    set obj [get_report_configs -of_objects [get_runs impl_1] $DESIGN\_impl_1_route_report_utilization]
     if { $obj != "" } {
 	Info CreateProject 1 "Report utilization created successfully"	
     }

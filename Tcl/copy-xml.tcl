3879f133b75b50dafe597c4283bf4a647d739fd6
Francesco Gonnella <francesco.gonnella@cern.ch>
2018-10-17 15:08:42 +0100

    Create copy-xml.tcl script

---
+++ Tcl/copy-xml.tcl
@@ -0,0 +1,24 @@
+#!/usr/bin/tclsh
+set tcl_path [file dirname [info script]]
+set repo_path $tcl_path/../..
+
+if {$argc != 2} {
+    puts "Usage: $argv0 <XML list file> <destination directory>"
+    exit
+}
+
+source $tcl_path/hog.tcl
+
+set list_file [lindex $argv 0]
+set dst       [lindex $argv 1]
+
+if {[file exists $list_file]} {
+    if ![file exists $dst] {
+	Warning CopyXML 0 "$dst directory not found, creating it..."
+	file mkdir $dst
+    }
+} else {
+    Error CopyXML 0 "$list_file not found"
+    exit
+}
+CopyXMLsFromListFile $list_file $tcl_path $dst 

61e59be260ea99ed797ada5411cc6ee6452cb95e
Francesco Gonnella <francesco.gonnella@cern.ch>
2019-02-28 16:39:10 +0100

    Create new scripts

--- Tcl/launch_implementation.tcl
+++ Tcl/launch_implementation.tcl
@@ -1,4 +1,4 @@
-set Name launch_synthesis
+set Name LaunchRuns
 set path [file normalize [file dirname [info script]]]
 if { $::argc eq 0 } {
     puts "USAGE: $::argv0 <project>"
@@ -6,22 +6,23 @@ if { $::argc eq 0 } {
 } else {
     set project [lindex $argv 0]
 	set main_folder [file normalize "$path/../../VivadoProject/$project/$project.runs/"]
-	set NJOBS 4
 }
 
 set old_path [pwd]
 cd $path
 source ./hog.tcl
 Info $Name 1 "Number of jobs set to $NJOBS."
+
 set commit [GetHash ALL ../../]
 
-Info $Name 2 "Running project script: $project.tcl..."
-open_project ../../VivadoProject/$project/$project.xpr
+Info $Name 2 "Opening project: $project.tcl..."
+open_project ../../VivadoProject/$project/$project.tcl
 
-Info $Name 3 "Starting complete design flow..."
-launch_runs synth_1  -jobs $NJOBS -dir $main_folder
-wait_on_run synth_1
-puts [get_property PROGRESS [get_runs synth_1]]
-puts [get_property STATUS [get_runs synth_1]]
-puts $Name 4 "All done."
+Info $Name 5 "Starting implementation flow..."
+
+launch_runs impl_1 -to_step write_bitstream -jobs $NJOBS -dir $main_folder
+wait_on_run impl_1
+puts [get_property PROGRESS [get_runs impl_1]]
+puts [get_property STATUS [get_runs impl_1]]
+
 cd $old_path
--- Tcl/launch_ip_synth.tcl
+++ Tcl/launch_ip_synth.tcl
@@ -1,4 +1,4 @@
-set Name launch_synthesis
+set Name LaunchRuns
 set path [file normalize [file dirname [info script]]]
 if { $::argc eq 0 } {
     puts "USAGE: $::argv0 <project>"
@@ -6,22 +6,28 @@ if { $::argc eq 0 } {
 } else {
     set project [lindex $argv 0]
 	set main_folder [file normalize "$path/../../VivadoProject/$project/$project.runs/"]
-	set NJOBS 4
 }
 
 set old_path [pwd]
 cd $path
 source ./hog.tcl
 Info $Name 1 "Number of jobs set to $NJOBS."
-set commit [GetHash ALL ../../]
 
 Info $Name 2 "Running project script: $project.tcl..."
-open_project ../../VivadoProject/$project/$project.xpr
+source -notrace ../../Top/$project/$project.tcl
+Info $Name 3 "Upgrading IPs if any..."
+set ips [get_ips *]
+if {$ips != ""} {
+    upgrade_ip $ips
+}
 
-Info $Name 3 "Starting complete design flow..."
-launch_runs synth_1  -jobs $NJOBS -dir $main_folder
-wait_on_run synth_1
-puts [get_property PROGRESS [get_runs synth_1]]
-puts [get_property STATUS [get_runs synth_1]]
-puts $Name 4 "All done."
+foreach ip in $ips {
+    puts "Launching run for $ip..."
+    launch_runs [get_runs $ip*]  -dir $main_folder
+    wait_on_run [get_runs $ip*]
+    puts [get_property PROGRESS [get_runs $ip*]]
+    puts [get_property STATUS [get_runs $ip*]]
+}
+
+
 cd $old_path

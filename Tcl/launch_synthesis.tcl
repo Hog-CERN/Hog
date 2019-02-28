5634ff10627af207617e0c86b9f38b559fd2129e
Francesco Gonnella <francesco.gonnella@cern.ch>
2019-02-28 16:15:26 +0100

    Create new script for CI

--- Tcl/launch_synthesis.tcl
+++ Tcl/launch_synthesis.tcl
@@ -1,4 +1,5 @@
 set Name LaunchRuns
+set path [file normalize [file dirname [info script]]]
 if { $::argc eq 0 } {
     puts "USAGE: $::argv0 <project> \[output directory\] \[number of jobs\]"
     exit 1
@@ -7,7 +8,7 @@ if { $::argc eq 0 } {
     if { $::argc > 1 } {
 	set main_folder [file normalize [lindex $argv 1]]
     } else {
-	set main_folder [file normalize "../../VivadoProject/$project/$project.runs/"]
+	set main_folder [file normalize "$path/../../VivadoProject/$project/$project.runs/"]
     }
 
     if { $::argc > 2 } {
@@ -24,7 +25,6 @@ if { $::argc eq 0 } {
 }
 
 set old_path [pwd]
-set path [file normalize [file dirname [info script]]]
 cd $path
 source ./hog.tcl
 Info $Name 1 "Number of jobs set to $NJOBS."
@@ -49,5 +49,14 @@ if {$no_time == 1 } {
     close $cfile
 }
 Info $Name 5 "Starting complete design flow..."
+launch_runs synth_1  -jobs $NJOBS -dir $main_folder
+wait_on_run synth_1
+puts [get_property PROGRESS [get_runs synth_1]]
+puts [get_property STATUS [get_runs synth_1]]
+
 launch_runs impl_1 -to_step write_bitstream -jobs $NJOBS -dir $main_folder
+wait_on_run impl_1
+puts [get_property PROGRESS [get_runs impl_1]]
+puts [get_property STATUS [get_runs impl_1]]
+
 cd $old_path

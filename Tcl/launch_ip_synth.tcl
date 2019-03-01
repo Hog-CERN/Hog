0274cadb2d8276c1494a4cf7b5350a027d8620b4
Francesco Gonnella <francesco.gonnella@cern.ch>
2019-03-01 13:21:59 +0100

    Debug

--- Tcl/launch_ip_synth.tcl
+++ Tcl/launch_ip_synth.tcl
@@ -17,24 +17,28 @@ open_project ../../VivadoProject/$project/$project.xpr
 
 
 Info $Name 2 "Preparing runs..."
+reset_run synth_1
 launch_runs -scripts_only synth_1
+reset_run synth_1
 
 set ips [get_ips *]
 foreach ip $ips {
     Info $Name 3 "Launching run for $ip..."
     if { [get_runs $ip\_synth_1] != "" } {
-	set run_name [get_runs $ip\_synth_1]
         set run_name [get_runs $ip\_synth_1]
+        set run_name [get_runs $ip\_synth_1]
+
+        reset_run $run_name
         launch_runs $run_name -dir $main_folder
         wait_on_run $run_name
         puts [get_property PROGRESS $run_name]
         puts [get_property STATUS $run_name]
     } else {
-	Warning $Name 3 "No run found for $ip."
+        Warning $Name 3 "No run found for $ip."
     }
 }
 
-launch_runs synth_1 -dir $main_folder -jobs 4  
+launch_runs synth_1 -dir $main_folder -jobs 4
 wait_on_run synth_1
 
 cd $old_path

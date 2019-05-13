f1a80b00de7da039b56d6e44593463bec60bf3a9
Francesco Gonnella <francesco.gonnella@cern.ch>
2019-05-13 15:34:57 +0100

    Disable check timing in post write bistream, for the time being

--- Tcl/post-bitstream.tcl
+++ Tcl/post-bitstream.tcl
@@ -62,29 +62,29 @@ if [file exists $bit_file] {
     file copy -force $run_dir/versions $dst_dir
     
     # Check timing
-    set wns [get_property STATS.WNS [get_runs impl_1]]
-    set tns [get_property STATS.TNS [get_runs impl_1]]
-    set whs [get_property STATS.WHS [get_runs impl_1]]
-    set ths [get_property STATS.THS [get_runs impl_1]]    
-    
-    if {$wns == 0 && $whs == 0} {
-	Info $NAME 7 "Time requirements are met"
-	set status_file [open "$dst_dir/timing_ok.txt" "w"]
-    } else {
-	CriticalWarning $NAME 7 "Time requirements are NOT met"
-	set status_file [open "$dst_dir/timing_error.txt" "w"]
-    }
-
-    Status $NAME 8 "WNS: $wns"
-    Status $NAME 8 "TNS: $tns"
-    Status $NAME 8 "WHS: $whs"
-    Status $NAME 8 "THS: $ths"
-
-    puts $status_file "WNS: $wns"
-    puts $status_file "TNS: $tns"
-    puts $status_file "WHS: $whs"
-    puts $status_file "THS: $ths"        
-    close $status_file
+    # set wns [get_property STATS.WNS [get_runs impl_1]]
+    # set tns [get_property STATS.TNS [get_runs impl_1]]
+    # set whs [get_property STATS.WHS [get_runs impl_1]]
+    # set ths [get_property STATS.THS [get_runs impl_1]]    
+    # 
+    # if {$wns == 0 && $whs == 0} {
+    # 	Info $NAME 7 "Time requirements are met"
+    # 	set status_file [open "$dst_dir/timing_ok.txt" "w"]
+    # } else {
+    # 	CriticalWarning $NAME 7 "Time requirements are NOT met"
+    # 	set status_file [open "$dst_dir/timing_error.txt" "w"]
+    # }
+    # 
+    # Status $NAME 8 "WNS: $wns"
+    # Status $NAME 8 "TNS: $tns"
+    # Status $NAME 8 "WHS: $whs"
+    # Status $NAME 8 "THS: $ths"
+    # 
+    # puts $status_file "WNS: $wns"
+    # puts $status_file "TNS: $tns"
+    # puts $status_file "WHS: $whs"
+    # puts $status_file "THS: $ths"        
+    # close $status_file
 
 } else {
     CriticalWarning $NAME 7 "Bit file not found."

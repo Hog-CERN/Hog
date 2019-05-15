6f944ef7dc7cfc654cf442c549a042d160446253
Francesco Gonnella <francesco.gonnella@cern.ch>
2019-05-15 09:26:48 +0100

    Add write bistream job

---
+++ Tcl/launch_write_bitstream.tcl
@@ -0,0 +1,30 @@
+set Name LaunchWriteBitstream
+set path [file normalize [file dirname [info script]]]
+if { $::argc eq 0 } {
+    puts "USAGE: $::argv0 <project>"
+    exit 1
+} else {
+    set project [lindex $argv 0]
+	set main_folder [file normalize "$path/../../VivadoProject/$project/$project.runs/"]
+}
+
+set old_path [pwd]
+cd $path
+source ./hog.tcl
+
+set commit [GetHash ALL ../../]
+
+Info $Name 2 "Opening $project..."
+open_project ../../VivadoProject/$project/$project.xpr
+
+Info $Name 5 "Starting write bitstream flow..."
+
+launch_runs impl_1 -to_step write_bitstream -jobs 4 -dir $main_folder
+wait_on_run impl_1
+
+set prog [get_property PROGRESS [get_runs impl_1]]
+set status [get_property STATUS [get_runs impl_1]]
+Info $Name 6 "Run: impl_1 progress: $prog, status : $status"
+
+Info $Name 7 "All done."
+cd $old_path

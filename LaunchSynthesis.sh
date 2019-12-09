3e54dfcaf874574083e593dd5dfd2476d3997ce1
Davide Cieri <davide.cieri@cern.ch>
2019-12-09 13:50:25 +0100

    Usage print screen if no proj specified and some instructions in the README

--- LaunchSynthesis.sh
+++ LaunchSynthesis.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-vivado -mode batch -notrace -source $DIR/Tcl/launchers/launch_synthesis.tcl -tclargs $1
+if [ -z "$1" ]
+then
+	printf "Project name has not been specified. Usage: \n ./Hog/LaunchSynthesis.sh <proj_name>\n"
+else
+	vivado -mode batch -notrace -source $DIR/Tcl/launchers/launch_synthesis.tcl -tclargs $1
+fi

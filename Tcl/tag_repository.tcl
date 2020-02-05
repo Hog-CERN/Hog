b9916bddd36763a5c266c602635579400e315990
Davide Cieri <davide.cieri@cern.ch>
2020-02-05 11:18:17 +0100

    Do not crash everything if use doxygen is not set

--- Tcl/tag_repository.tcl
+++ Tcl/tag_repository.tcl
@@ -27,56 +27,59 @@ Msg Info "Old tag was: $old_tag and new tag is: $new_tag"
 if {$version_level >= 3} {
     set official $env(HOG_OFFICIAL_BIN_EOS_PATH)
     set unofficial $env(HOG_UNOFFICIAL_BIN_EOS_PATH)
-    set use_doxygen $env(HOG_USE_DOXYGEN)
-    # Run doxygen
-    set doxygen_conf "./doxygen/doxygen.conf"
+    
+    if {[info exists $env(HOG_USE_DOXYGEN)]} {
+    	set use_doxygen $env(HOG_USE_DOXYGEN)
+    
+	    # Run doxygen
+	    set doxygen_conf "./doxygen/doxygen.conf"
 
-    if {$use_doxygen == 1} {
-	if {[file exists $doxygen_conf] & [DoxygenVersion 1.8.13]} {
-	    Msg Info "Running doxygen with $doxygen_conf..."
-	    set outfile [open $doxygen_conf a]
-	    puts $outfile \nPROJECT_NUMBER=$new_tag
-	    close $outfile
-	    exec -ignorestderr doxygen $doxygen_conf
-	} else {
-	    Msg Info "Could not find $doxygen_conf or Doxygen version is older than 1.8.13. Will not run doxygen."
-	}
-    }
-    set wild_card $unofficial/*$old_tag*
-    set status [catch {exec eos ls $wild_card} folders]
-    if {$status == 0} {
-	Msg Info "Found these files using $wild_card: $folders"
-	Msg Info "Copying files to official directory..."
-	set new_dir $official/$new_tag
-
-	Msg Info "Creating $new_dir"
-	exec eos mkdir -p $new_dir
-
-
-	# Copying doxygen documentation if files were created
-	if {[file exists ../Doc/html]} {
-	    set dox_dir $official/$new_tag/doc
-	    Msg Info "Creating doxygen dir $dox_dir..."
-	    exec eos mkdir -p $dox_dir
-	    Msg Info "Copying doxygen files..."
-	    exec -ignorestderr eos cp -r ../Doc/html/* $dox_dir
-	}
-	foreach f $folders {
-	    set dst $new_dir/[regsub "(.*)_$old_tag\(.*\)" $f "\\1\\2"]
-	    Msg Info "Copying $f into $dst..."
-	    exec eos mkdir -p $dst
-	    exec -ignorestderr eos cp -r $unofficial/$f/* $dst/
-	    
-	    Msg Info "Ranaming bit and bin files..."
-	    catch {exec eos ls $dst/*$old_tag.*} new_files
-	    foreach ff $new_files {
-		set old_name $dst/$ff
-		set new_name [regsub "(.*)$old_tag\(.*\)" $old_name "\\1$new_tag\\2"]
-		Msg Info "Moving $old_name into $new_name..."
-		exec eos mv $old_name $new_name
+	    if {$use_doxygen == 1} {
+		if {[file exists $doxygen_conf] & [DoxygenVersion 1.8.13]} {
+		    Msg Info "Running doxygen with $doxygen_conf..."
+		    set outfile [open $doxygen_conf a]
+		    puts $outfile \nPROJECT_NUMBER=$new_tag
+		    close $outfile
+		    exec -ignorestderr doxygen $doxygen_conf
+		} else {
+		    Msg Info "Could not find $doxygen_conf or Doxygen version is older than 1.8.13. Will not run doxygen."
+		}
 	    }
-	}
+	    set wild_card $unofficial/*$old_tag*
+	    set status [catch {exec eos ls $wild_card} folders]
+	    if {$status == 0} {
+		Msg Info "Found these files using $wild_card: $folders"
+		Msg Info "Copying files to official directory..."
+		set new_dir $official/$new_tag
 
+		Msg Info "Creating $new_dir"
+		exec eos mkdir -p $new_dir
+
+
+		# Copying doxygen documentation if files were created
+		if {[file exists ../Doc/html]} {
+		    set dox_dir $official/$new_tag/doc
+		    Msg Info "Creating doxygen dir $dox_dir..."
+		    exec eos mkdir -p $dox_dir
+		    Msg Info "Copying doxygen files..."
+		    exec -ignorestderr eos cp -r ../Doc/html/* $dox_dir
+		}
+		foreach f $folders {
+		    set dst $new_dir/[regsub "(.*)_$old_tag\(.*\)" $f "\\1\\2"]
+		    Msg Info "Copying $f into $dst..."
+		    exec eos mkdir -p $dst
+		    exec -ignorestderr eos cp -r $unofficial/$f/* $dst/
+		    
+		    Msg Info "Ranaming bit and bin files..."
+		    catch {exec eos ls $dst/*$old_tag.*} new_files
+		    foreach ff $new_files {
+			set old_name $dst/$ff
+			set new_name [regsub "(.*)$old_tag\(.*\)" $old_name "\\1$new_tag\\2"]
+			Msg Info "Moving $old_name into $new_name..."
+			exec eos mv $old_name $new_name
+		    }																
+		}
+	}
     # Delete unofficial tags
     Msg Info "Looking for unofficial tags to delete for official version $new_tag..."
     set res [ catch {exec git tag {*}"-l b*$new_tag*"} tags_to_delete]

8c38b67513f04e81bfbb36b6498b9f8dfea5e5cd
Davide Cieri <davide.cieri@cern.ch>
2020-02-11 14:53:58 +0100

    removing xml scripts

--- Init.sh
+++ Init.sh
@@ -8,7 +8,7 @@ if [ "$1" == "-h" ] || [ "$1" == "-help" ] || [ "$1" == "--help" ] || [ "$1" == 
     echo " ---------------------------"
     echo " Initialise your Hog-handled firmware repository"
     echo " - Create links to Hog git-hooks"
-    echo " - Locally ignore Xilinx IP xml files"
+    # echo " - Locally ignore Xilinx IP xml files"
     echo " - Initialise and update your submodules"
     echo " - (optional) Compile questasim libraries (if questasim executable is found)"
     echo " - (optional) Create vivado projects (if vivado exacutable is found)"
@@ -18,28 +18,28 @@ fi
 
 cd "${DIR}"
 
-echo [hog init] Creating links to hooks...
-cd ../.git/hooks
-for h in `ls ../../Hog/git-hooks/*`
-do
-    ln -s "${h}"
-done
+# echo [hog init] Creating links to hooks...
+# cd ../.git/hooks
+# for h in `ls ../../Hog/git-hooks/*`
+# do
+#     ln -s "${h}"
+# done
 
-cd ../..
+# cd ../..
 
-echo [hog init] Ignoring Xilinx IP xml locally
-xci=`find . -name *.xci`
-for f in "${xci}"
-do
-    ext="${f##*.}"
-    name="${f%.*}"
-    if [ -e "$name.xml" ]
-    then
-	echo [git init] Assuming unchanged: $name.xml
-	git update-index --assume-unchanged "${name}".xml
-    fi
-done
-cd "${DIR}"
+# echo [hog init] Ignoring Xilinx IP xml locally
+# xci=`find . -name *.xci`
+# for f in "${xci}"
+# do
+#     ext="${f##*.}"
+#     name="${f%.*}"
+#     if [ -e "$name.xml" ]
+#     then
+# 	echo [git init] Assuming unchanged: $name.xml
+# 	git update-index --assume-unchanged "${name}".xml
+#     fi
+# done
+# cd "${DIR}"
 
 if [ `which vivado` ]
 then

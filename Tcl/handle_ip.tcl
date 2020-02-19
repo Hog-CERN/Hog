053883c09eabeb924d38f15c07736a8902bf740b
Francesco Gonnella <fgonnell@atlas-tdaq-firmware-dev.cern.ch>
2020-02-19 12:50:10 +0100

    handle_ip now works

--- Tcl/handle_ip.tcl
+++ Tcl/handle_ip.tcl
@@ -1,16 +1,22 @@
 #!/usr/bin/env tclsh
-if { $::argc eq 0 } {
-    puts "USAGE: $::argv0 <IP file.xci> \[IP repository path (on eos)\]"
+if { $::argc < 2 } {
+    puts "USAGE: $::argv0 <push|pull> <IP file.xci> \[IP repository path (on eos)\]"
     exit 1
 } else {
-    set xci_file [file normalize [lindex $argv 0]]
-    if { $::argc > 1 } {
-	set ip_path [lindex $argv 1]
+    set what_to_do [lindex $argv 0]
+    if {!($what_to_do eq "push") && !($what_to_do eq "pull")} {
+	puts "ERROR: you must specify push or pull as first argument\n\n"
+	exit -1
+    }
+
+    set xci_file [file normalize [lindex $argv 1]]
+    if { $::argc > 2 } {
+	set ip_path [lindex $argv 2]
     } else {
 	if [info exists env(HOG_IP_EOS_PATH)] {
 	    set ip_path $env(HOG_IP_EOS_PATH)
 	} else {
-	    puts "ERROR: environment variable HOG_IP_EOS_PATH is not defined. Please define it or specify a path as an argument to $::argv0."
+	    puts "ERROR: environment variable HOG_IP_EOS_PATH is not defined. Please define it or specify a path as an argument to $::argv0.\n\n"
 	    exit -1
 	}
     }
@@ -22,7 +28,7 @@ source ./hog.tcl
 
 set ip_path_path [file normalize $ip_path/..]
 if  {[catch {exec eos ls $ip_path_path} result]} {
-    Msg Error "Could not find mother directory for $ip_path: $ip_path_path."
+    Msg Error "Could not find mother directory for $ip_path: $ip_path_path.\n\n"
     exit -1
 } else {
     if  {[catch {exec eos ls $ip_path} result]} {
@@ -34,7 +40,7 @@ if  {[catch {exec eos ls $ip_path_path} result]} {
 }
 
 if !([file exists $xci_file]) {
-    Msg Error "Could not find $xci_file"
+    Msg Error "Could not find $xci_file.\n\n"
     exit -1
 }
 
@@ -46,16 +52,25 @@ set hash [lindex [exec md5sum $xci_file] 0]
 set file_name $xci_name\_$hash
 
 
-if  {[catch {exec eos ls $ip_path/$file_name} result]} {
-    Msg Info "IP not found in the repository, copying it over..."
+if {$what_to_do eq "push"} {
+    if  {[catch {exec eos ls $ip_path/$file_name} result]} {
+	Msg Info "IP not found in the repository, copying it over..."
+    } else {
+	Msg Warning "IP already in the repository, replacing..."
+	exec -ignorestderr eos rm -r $ip_path/$file_name
+    }
     exec -ignorestderr eos cp -r $xci_path $ip_path
     exec eos mv $ip_path/$xci_dir_name $ip_path/$file_name
+} elseif {$what_to_do eq "pull"} {
+    if  {[catch {exec eos ls $ip_path/$file_name} result]} {
+	Msg Error "IP not found in the repository, cannot pull.\n\n"
+	exit -1
+	
+    } else {
+	Msg Info "IP found in the repository, copying it locally..."
+	exec -ignorestderr eos cp -r $ip_path/$file_name/* $xci_path
+    } 
+}
 
-} else {
-    Msg Info "IP found in the repository, copying it locally..."
-    exec -ignorestderr eos cp -r $ip_path/$file_name/* $xci_path
-} 
-
-#exec -ignorestderr eos cp -r $unofficial/$f/* $dst/
-
+Msg Info "All done."
 cd $old_path

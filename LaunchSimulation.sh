03ce4f97aa0ece2e0757121f6c891df4e751298b
Davide Cieri <davide.cieri@cern.ch>
2020-02-24 12:04:35 +0100

    launch simulation script looks into .sim file to get the right simulatior

--- LaunchSimulation.sh
+++ LaunchSimulation.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ -z "$1" ] || [  -z "$2" ]
+if [ -z "$1" ]
 then
-	printf "Project name has not been specified. Usage: \n Hog/LaunchSimulation.sh <proj_name> <simulator> [library path]\n"
-	printf "Possible choices for <simulator> are: vivadosim, modelsim\n"
+    printf "Project name has not been specified. Usage: \n Hog/LaunchSimulation.sh <proj_name> [library path]\n"
 else
-	if [ $2 == "vivadosim" ] 
-	then
-		vivado -mode batch -notrace -source $DIR/Tcl/launchers/launch_vivado_simulation.tcl -tclargs $1 $3
-	else
-        vivado -mode batch -notrace -source $DIR/Tcl/launchers/launch_simulation.tcl -tclargs $1 $3
-	fi
+    vivado -mode batch -notrace -source $DIR/Tcl/launchers/launch_simulation.tcl -tclargs $1 $2
 fi

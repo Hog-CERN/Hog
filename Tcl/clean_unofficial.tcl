bd2762c9e9708a627d91c881ea9affd1f03c9811
dcieri <davide.cieri@cern.ch>
2020-03-06 13:14:18 +0100

    cleaning tcl script

---
+++ Tcl/clean_unofficial.tcl
@@ -0,0 +1,32 @@
+#!/usr/bin/env tclsh
+set Name clean_unofficial
+
+set old_path [pwd]
+set path [file dirname [info script]]
+cd $path
+source ./hog.tcl
+cd ../../
+
+set master_shas [exec git rev-parse --short master --all]
+Msg Info "Retrieving list of commits for 'master' branch..."
+set list_master_shas [split $master_shas "\n"]
+puts $list_master_shas
+
+set unofficial $env(HOG_UNOFFICIAL_BIN_EOS_PATH)
+Msg Info "Retrieving list of bitfiles in $unofficial..."
+set bitfiles [exec eos ls $unofficial]
+set list_bitfiles [split $bitfiles "\n"]
+puts $list_bitfiles
+
+foreach sha $list_master_shas {
+    foreach bitfile $list {
+        if {$sha == $bitfile} {
+            Msg Info "Removing files corresponding to SHA $sha"
+            exec eos rm -r $unofficial/$sha
+        }
+    }
+}
+
+Msg Info "Cleaning done"
+
+cd $old_path

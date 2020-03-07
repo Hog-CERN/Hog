f4219e7dcc18bfab36c1d0178cee19d13d98cf2d
hog <hdl.on.git@cern.ch>
2020-03-07 14:16:18 +0100

    Fix if on ip files on eos

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -1431,15 +1431,15 @@ proc HandleIP {what_to_do xci_file ip_path runs_dir} {
 	    set ret_s [catch {exec eos ls $ip_path/$file_name/synthesized/*} ip_syn_files]
 
 	    if  {($ret_g == 0) && ([llength $ip_gen_files] > 0)} {
-		Msg Warning "Cound not find generated IP files on EOS path" 
-	    } else {
 		exec -ignorestderr eos cp -r $ip_path/$file_name/generated/* $xci_path
+	    } else {
+		Msg Warning "Cound not find generated IP files on EOS path" 
 	    }
 
 	    if  {($ret_s == 0) && ([llength $ip_syn_files] > 0)} {
-		Msg Warning "Cound not find synthesized IP files on EOS path" 
-	    } else {
 		exec -ignorestderr eos cp -r $ip_path/$file_name/synthesized/* $runs_dir	
+	    } else {
+		Msg Warning "Cound not find synthesized IP files on EOS path" 
 	    }
 
 	

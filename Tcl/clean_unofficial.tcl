e728d9d09cfbe3f8790d76320676bd952139ea9a
dcieri <davide.cieri@cern.ch>
2020-03-09 10:11:22 +0100

    removed apices

--- Tcl/clean_unofficial.tcl
+++ Tcl/clean_unofficial.tcl
@@ -7,25 +7,26 @@ cd $path
 source ./hog.tcl
 cd ../../
 
-set master_shas [exec git log --pretty=format:"%h"]
+set master_shas [exec git log --pretty=format:%h]
 Msg Info "Retrieving list of commits for 'master' branch..."
 set list_master_shas [split $master_shas "\n"]
 puts $list_master_shas
 
-set unofficial $env(HOG_UNOFFICIAL_BIN_EOS_PATH)
-Msg Info "Retrieving list of bitfiles in $unofficial..."
-set bitfiles [exec eos ls $unofficial]
-set list_bitfiles [split $bitfiles "\n"]
-puts $list_bitfiles
+#set unofficial $env(HOG_UNOFFICIAL_BIN_EOS_PATH)
+#Msg Info "Retrieving list of bitfiles in $unofficial..."
+#set bitfiles [exec eos ls $unofficial]
+#set list_bitfiles [split $bitfiles "\n"]
+#puts $list_bitfiles
 
 foreach sha $list_master_shas {
-    foreach bitfile $list_bitfiles {
-        if {[string first $sha $bitfile] != -1} {
-            Msg Info "Removing files corresponding to SHA $bitfile"
-            set status [catch {exec eos rm -r $unofficial/$bitfile} deletion]
-            puts $status
-        }
-    }
+	puts $sha
+#    foreach bitfile $list_bitfiles {
+#        if {[string first $sha $bitfile] != -1} {
+#            Msg Info "Removing files corresponding to SHA $bitfile"
+#            set status [catch {exec eos rm -r $unofficial/$bitfile} deletion]
+#            puts $status
+#        }
+#    }
 }
 
 Msg Info "Cleaning done"

a95785ff581feeac64cdf138df0a1d48d41dbee6
dcieri <davide.cieri@cern.ch>
2020-03-09 13:38:48 +0100

    clean uses just one loop, first commit for tag CI

--- Tcl/clean_unofficial.tcl
+++ Tcl/clean_unofficial.tcl
@@ -7,9 +7,9 @@ cd $path
 source ./hog.tcl
 cd ../../
 
-set master_shas [exec git log --pretty=format:%h]
-Msg Info "Retrieving list of commits for 'master' branch..."
-set list_master_shas [split $master_shas "\n"]
+#set master_shas [exec git log --pretty=format:%h]
+#Msg Info "Retrieving list of commits for 'master' branch..."
+#set list_master_shas [split $master_shas "\n"]
 # puts $list_master_shas
 
 set unofficial $env(HOG_UNOFFICIAL_BIN_EOS_PATH)
@@ -18,13 +18,10 @@ set bitfiles [exec eos ls $unofficial]
 set list_bitfiles [split $bitfiles "\n"]
 # puts $list_bitfiles
 
-foreach sha $list_master_shas {
-	puts $sha
-    foreach bitfile $list_bitfiles {
-       if {[string first $sha $bitfile] != -1} {
-           Msg Info "Removing files corresponding to SHA $bitfile"
-           set status [catch {exec eos rm -r $unofficial/$bitfile} deletion]
-       }
+foreach bitfile $list_bitfiles {
+   if {[exec git branch master --contains $bitfile] != ""} {
+       Msg Info "Removing files corresponding to SHA $bitfile"
+       set status [catch {exec eos rm -r $unofficial/$bitfile} deletion]
    }
 }
 

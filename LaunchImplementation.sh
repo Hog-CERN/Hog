1a7af65aaded36c8de83dc2a846713d751237c2f
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-03-12 13:47:50 +0000

    Add second argument to LaunchImplementation.sh to sto write bitstream

--- LaunchImplementation.sh
+++ LaunchImplementation.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -z "$1" ]
 then
-    printf "Project name has not been specified. Usage: \n ./Hog/               LaunchImplementation.sh <proj_name>\n"
+    printf "Project name has not been specified. Usage: \n ./Hog/LaunchImplementation.sh <proj_name> [1|0]\n A 1 as second argument will stop implementation before write bitstream. Default is 0.\n"
 else
-	vivado -nojournal -nolog -mode batch -notrace -source $DIR/Tcl/launchers/launch_implementation.tcl -tclargs $1
+	vivado -nojournal -nolog -mode batch -notrace -source $DIR/Tcl/launchers/launch_implementation.tcl -tclargs $@
 fi

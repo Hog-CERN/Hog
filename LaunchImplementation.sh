bbc414db543ffcaa292baaf5b9908d49bc2cf09a
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-03-12 14:28:56 +0000

    Better with a more explicit option

--- LaunchImplementation.sh
+++ LaunchImplementation.sh
@@ -2,7 +2,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -z "$1" ]
 then
-    printf "Project name has not been specified. Usage: \n ./Hog/LaunchImplementation.sh <proj_name> [1|0]\n A 1 as second argument will stop implementation before write bitstream. Default is 0.\n"
+    printf "Project name has not been specified. Usage: \n ./Hog/LaunchImplementation.sh <proj_name> [-nobitstream]\n"
 else
-	vivado -nojournal -nolog -mode batch -notrace -source $DIR/Tcl/launchers/launch_implementation.tcl -tclargs $@
+    if [[ $2 == "-nobitstream" ]]; then
+	nobs=1
+    fi
+    vivado -nojournal -nolog -mode batch -notrace -source $DIR/Tcl/launchers/launch_implementation.tcl -tclargs $1 $nobs
 fi

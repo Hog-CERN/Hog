e8d4a56a7d735cae57c5d18e67cc9a6a8ab8c6fe
dcieri <davide.cieri@cern.ch>
2020-03-20 16:52:20 +0100

    let's copy

--- Tcl/copy_to_eos.tcl
+++ Tcl/copy_to_eos.tcl
@@ -31,38 +31,54 @@ lassign [ExtractVersionFromTag $tag] M m p mr
 
 if {$mr == -1} {
     set wild_card $unofficial/$current_sha/
-    
     lassign [eos "ls $wild_card"] status folders
-    
+
     if {$status == 0} {
         Msg Info "Found these files using $wild_card: $folders"
-        Msg Info "Copying files to official directory..."
         set new_dir $official/$tag
+        Msg Info "Copying $wild_card into $official/$new_dir"
+        eos "cp $wild_card $official" 5
+        eos "mv $official/$current_sha $official/$new_dir" 5
+        # eos "mkdir -p $new_dir" 5
+        # puts "eos mkdir -p $new_dir"
+        puts "eos cp $wild_card $official"
+        puts "eos mv $official/$current_sha $official/$new_dir"
 
-        Msg Info "Creating $new_dir"
-        eos "mkdir -p $new_dir" 5
-        
         # f Loop over projects in repository
         foreach f $folders {
             set new_folder [string range $f 0 [expr {[string last "-git" $f]}]]
-            Msg Info "Copying $f into $official/$tag/$new_folder\_$tag..."
-            eos "mkdir -p $official/$tag/$new_folder\_$tag"
-            foreach fp $f {
-                puts $fp
-                set new_fp [string range $fp 0 [expr {[string last "-git" $fp]} - 1]]
-                set extension [string range $fp [expr {[string last "." $fp]} + 1] end]
-                if {[string last "-git" $fp] != -1} {
-                    Msg Info "eos cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder$tag/$new_fp.$extension"
-                    eos "cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder\_$tag/$new_fp.$extension" 5 
-                } else {
-                    Msg Info "eos cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder$tag/$fp"
-                    eos "cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder\_$tag/$fp" 5 
+            if { new_folder != ""} {
+                Msg Info "Renaming $f into $new_folder" 
+                eos "mv $official/$new_dir/$f $official/$new_dir/$new_folder" 5
+                lassign [eos "ls $official/$new_dir/$new_folder"] sub_status sub_folders
+                foreach fp $sub_folders {
+                    set new_file [string range $fp 0 [expr {[string last "-git" $fp]}]]
+                    set extension [string range $fp [expr {[string last "." $fp]} + 1] end]
+                    if {new_file != ""} {
+                        Msg Info "Renaming file $fp into $new_file.$extension"
+                        eos "mv $official/$new_dir/$new_folder/$fp $official/$new_dir/$new_folder/$new_file.$extension" 5
+                    }
                 }
+            } 
+            # Msg Info "Copying $f into $official/$tag/$new_folder\_$tag..."
+            # # eos "mkdir -p $official/$tag/$new_folder$tag"
+            # puts "eos mkdir -p $official/$tag/$new_folder$tag"
+            # foreach fp $f {
+            #     puts $fp
+            #     set new_fp [string range $fp 0 [expr {[string last "-git" $fp]} - 1]]
+            #     set extension [string range $fp [expr {[string last "." $fp]} + 1] end]
+            #     if {[string last "-git" $fp] != -1} {
+            #         Msg Info "eos cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder$tag/$new_fp.$extension"
+            #         # eos "cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder\_$tag/$new_fp.$extension" 5 
+            #     } else {
+            #         Msg Info "eos cp -r $unofficial/$current_sha/$fp/* $official/$tag/$new_folder$tag/$fp"
+            #         # eos "cp -r $unofficial/$current_sha/$fp/* $official/$tag/$" 5 
+            #     }
 
             }
         }
 
-        set wild_card $unofficial/$current_sha/Doc
+        set wild_card $unofficial/$current_sha/Doc*
         lassign [eos "ls $wild_card"] status doc_folder
 
         if {$status == 0} {

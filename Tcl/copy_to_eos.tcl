20e63613cb33a47cf191606d3ff3b94e96f7b0cd
dcieri <davide.cieri@cern.ch>
2020-03-21 10:12:04 +0100

    finally fixed copy_to_eos

--- Tcl/copy_to_eos.tcl
+++ Tcl/copy_to_eos.tcl
@@ -39,49 +39,28 @@ if {$mr == -1} {
         Msg Info "Copying $wild_card into $new_dir"
         eos "cp -r $wild_card $official" 5
         eos "mv -r $official/$current_sha $new_dir" 5
-        # eos "mkdir -p $new_dir" 5
-        # puts "eos mkdir -p $new_dir"
-
-        # f Loop over projects in repository
+         # f Loop over projects in repository
         foreach f $folders {
             set new_folder [string range $f 0 [expr {[string last "-git" $f]}]]
             if { $new_folder != ""} {
-                Msg Info "Renaming $f into $new_folder" 
-                eos "mv -r $new_dir/$f $new_dir/$new_folder" 5
-                lassign [eos "ls $new_dir/$new_folder"] sub_status sub_folders
+                Msg Info "Renaming $f into $new_folder$tag" 
+                eos "mv -r $new_dir/$f $new_dir/$new_folder$tag" 5
+                lassign [eos "ls $new_dir/$new_folder$tag"] sub_status sub_folders
                 foreach fp $sub_folders {
-                    set new_file [string range $fp 0 [expr {[string last "-git" $fp] -1}]]
+                    set new_file [string range $fp 0 [expr {[string last "-git" $fp]}]]
                     set extension [string range $fp [expr {[string last "." $fp]} + 1] end]
                     if {$new_file != ""} {
-                        Msg Info "Renaming file $fp into $new_file.$extension"
-                        eos "mv $new_dir/$new_folder/$fp $new_dir/$new_folder/$new_file.$extension" 5
+                        Msg Info "Renaming file $fp into $new_file$tag.$extension"
+                        eos "mv $new_dir/$new_folder$tag/$fp $new_dir/$new_folder$tag/$new_file$tag.$extension" 5
                     }
                 }
-            } 
-            # Msg Info "Copying $f into $official/$tag/$new_folder\_$tag..."
-            # # eos "mkdir -p $official/$tag/$new_folder$tag"
-            # puts "eos mkdir -p $official/$tag/$new_folder$tag"
-            # foreach fp $f {
-            #     puts $fp
-            #     set new_fp [string range $fp 0 [expr {[string last "-git" $fp]} - 1]]
-            #     set extension [string range $fp [expr {[string last "." $fp]} + 1] end]
-            #     if {[string last "-git" $fp] != -1} {
-            #         Msg Info "eos cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder$tag/$new_fp.$extension"
-            #         # eos "cp -r $unofficial/$current_sha/$fp $official/$tag/$new_folder\_$tag/$new_fp.$extension" 5 
-            #     } else {
-            #         Msg Info "eos cp -r $unofficial/$current_sha/$fp/* $official/$tag/$new_folder$tag/$fp"
-            #         # eos "cp -r $unofficial/$current_sha/$fp/* $official/$tag/$" 5 
-            #     }
-        }
-
-        set wild_card $unofficial/$current_sha/Doc*
-        lassign [eos "ls $wild_card"] status doc_folder
-
-        if {$status == 0} {
-            Msg Info "Updating official doxygen documentation in $official/Doc"
-            eos "mkdir -p $official/Doc" 5
-            eos "cp -r $unofficial/$current_sha/Doc*/* $official/Doc/" 5
-        }
+            }
+            if { $new_folder == "Doc-" } {
+                Msg Info "Updating official doxygen documentation in $official/Doc"    
+                eos "mkdir -p $official/Doc" 5
+                eos "cp -r $new_dir/$new_folder$tag/* $official/Doc/" 5           
+            }    
+       }
     } else {
         Msg Error "Could not find anything useful using $wild_card."
     }

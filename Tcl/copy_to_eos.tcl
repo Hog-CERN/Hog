8d6514b61ca9562de73a475f74c8779e7f05cb94
dcieri <davide.cieri@cern.ch>
2020-03-25 12:05:07 +0100

    using regexp

--- Tcl/copy_to_eos.tcl
+++ Tcl/copy_to_eos.tcl
@@ -41,24 +41,28 @@ if {$mr == -1} {
         eos "mv -r $official/$current_sha $new_dir" 5
          # f Loop over projects in repository
         foreach f $folders {
-            set new_folder [string range $f 0 [expr {[string last "-git" $f]}]]
+            set new_folder ""
+            regexp {(.*?)(?:-v\d+\.\d+\.\d+)?-\d+-g.+} $f a new_folder 
+            # set new_folder [string range $f 0 [expr {[string last "-git" $f]}]]
             if { $new_folder != ""} {
-                Msg Info "Renaming $f into $new_folder$tag" 
-                eos "mv -r $new_dir/$f $new_dir/$new_folder$tag" 5
-                lassign [eos "ls $new_dir/$new_folder$tag"] sub_status sub_folders
+                Msg Info "Renaming folder $f into $new_folder-$tag" 
+                eos "mv -r $new_dir/$f $new_dir/$new_folder-$tag" 5
+                lassign [eos "ls $new_dir/$new_folder-$tag"] sub_status sub_folders
                 foreach fp $sub_folders {
-                    set new_file [string range $fp 0 [expr {[string last "-git" $fp]}]]
+                    # set new_file [string range $fp 0 [expr {[string last "-git" $fp]}]]
+                    set new_file ""
+                    regexp {(.*?)(?:-v\d+\.\d+\.\d+)?-\d+-g.+} $fp a new_file 
                     set extension [string range $fp [expr {[string last "." $fp]} + 1] end]
                     if {$new_file != ""} {
-                        Msg Info "Renaming file $fp into $new_file$tag.$extension"
-                        eos "mv $new_dir/$new_folder$tag/$fp $new_dir/$new_folder$tag/$new_file$tag.$extension" 5
+                        Msg Info "Renaming file $fp into $new_file-$tag.$extension"
+                        eos "mv $new_dir/$new_folder-$tag/$fp $new_dir/$new_folder-$tag/$new_file-$tag.$extension" 5
                     }
                 }
             }
-            if { $new_folder == "Doc-" } {
+            if { $new_folder == "Doc" } {
                 Msg Info "Updating official doxygen documentation in $official/Doc"    
                 eos "mkdir -p $official/Doc" 5
-                eos "cp -r $new_dir/$new_folder$tag/* $official/Doc/" 5           
+                eos "cp -r $new_dir/$new_folder-$tag/* $official/Doc/" 5           
             }    
        }
     } else {

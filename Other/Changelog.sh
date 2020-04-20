bd675574ef5751c0b87139f0560bc1336ac08ed4
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-04-20 15:16:01 +0100

    debug Changelog

--- Other/Changelog.sh
+++ Other/Changelog.sh
@@ -6,11 +6,12 @@ else
 fi
 
 git rev-parse --verify ${TARGET_BRANCH} >/dev/null 2>&1
+SRC_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 if [ $? -eq 0 ]; then
     echo "## Changelog"
     echo
-    git log --no-merges 53-hog-tcl-cleanup ^${TARGET_BRANCH} --format=%B | grep FEATURE: | sed 's/FEATURE: */- /'
+    git log --no-merges ${SRC_BRANCH} ^${TARGET_BRANCH} --format=%B | grep FEATURE: | sed 's/FEATURE: */- /'
     echo 
 fi
 

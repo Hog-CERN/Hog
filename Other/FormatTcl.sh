6abd6201cd21fc66b81ca4ba551fadf314d64a32
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-05-01 12:33:41 +0100

    FEATURE: fix bug in FormatTcl: worked only if launche from Hog directory

--- Other/FormatTcl.sh
+++ Other/FormatTcl.sh
@@ -6,20 +6,23 @@
 #!/bin/bash
 
 tcl_dir=$1
+OLD_DIR=`pwd`
+THIS_DIR="$(dirname "$0")" 
 
 if [ ! -z "${tcl_dir}" ];then
     for f in ${tcl_dir}*; do
         if [ -d "$f" ]; then
             for file in $f/*.tcl; do
                 echo "[FormatTcl] Formatting $file..."
-                tclsh Tcl/utils/reformat.tcl -tab_width 2 $file
+                tclsh "${THIS_DIR}"/../Tcl/utils/reformat.tcl -tab_width 2 $file
             done
         fi
         if [[ $f == *.tcl ]]; then
             echo "[FormatTcl] Formatting $f..."
-            tclsh Tcl/utils/reformat.tcl -tab_width 2 $f
+            tclsh "${THIS_DIR}"/../Tcl/utils/reformat.tcl -tab_width 2 $f
         fi
     done
 else
-    printf "Project name has not been specified. Usage: \n ./Other/FormatTcl.sh <folder> \n"
+    printf "Folder name has not been specified. Usage: \n $0 <folder> \n"
 fi
+cd "${OLD_DIR}"

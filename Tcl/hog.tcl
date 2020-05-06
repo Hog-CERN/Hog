baf579a388f3eb70269ee608f89aa69e5aad9b95
dcieri <davide.cieri@cern.ch>
2020-05-06 10:58:50 +0200

    debug puts

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -1238,6 +1238,7 @@ proc HandleIP {what_to_do xci_file ip_path runs_dir {force 0}} {
 
       if  {($ret_s == 0) && ([llength $ip_syn_files] > 0)} {
         eos "cp -r $ip_path/$file_name/synthesized/* $runs_dir"
+        puts $runs_dir
       } else {
         Msg Warning "Cound not find synthesized IP files on EOS path"
       }

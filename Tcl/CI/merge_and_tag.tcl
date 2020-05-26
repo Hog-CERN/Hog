57460874700726330b442f3cf83785773a97a0d7
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-05-26 19:23:42 +0100

    Merge parameter are always passed to merge_and_tag.tcl, a new option -no_increase is used to ignore version increase if the branch is not the official one

--- Tcl/CI/merge_and_tag.tcl
+++ Tcl/CI/merge_and_tag.tcl
@@ -28,13 +28,14 @@ if {[catch {package require cmdline} ERROR]} {
 }
 
 set parameters {
-  {Hog    "Runs merge and tag of Hog repository. Default = off. To be only used by HOG developers!!!"}
-  {merged "Set this flag to tag the new version after merging the merge_request. Default = off"}
+  {Hog "Runs merge and tag of Hog repository. Default = off. To be only used by HOG developers!!!"}
+  {merged "If set, instructs this script to tag the new official version (of the form vM.m.p). To be used once the merge request is merged is merged Default = off"}
   {mr_par.arg "" "Merge request parameters in JSON format. Ignored if -merged is set"}
   {mr_id.arg 0 "Merge request ID. Ignored if -merged is set"}
   {push.arg "" "Optional: git branch for push"}
   {main_branch.arg "master" "Main branch (default = master)"}
-  {default_version_level.arg "patch" "Default version level to increase if nothing is psecified in the merge request description. Can be patch, minor, major (default = patch)"}
+  {default_version_level.arg "patch" "Default version level to increase if nothing is specified in the merge request description. Can be patch, minor, major. Default = patch"}
+  {no_increase "If set, prevents this script to increase the version if MAJOR_VARION, MINOR_VERSION or PATCH_VERSION directives are found in the merge request descritpion. Default = off"}
 }
 
 set usage "- CI script that merges your branch with \$HOG_TARGET_BRANCH and creates a new tag\n USAGE: $::argv0 \[OPTIONS\] \n. Options:"
@@ -73,36 +74,40 @@ if {$options(merged) == 0} {
   set MERGE_STATUS [ParseJSON  $options(mr_par) "merge_status"]
   set DESCRIPTION [ParseJSON  $options(mr_par) "description"]
   Msg Info "WIP: ${WIP},  Merge Request Status: ${MERGE_STATUS}   Description: ${DESCRIPTION}"
-  if {[lsearch $DESCRIPTION "*PATCH_VERSION*" ] >= 0} {
+  if {$options(no_increase) != 0} {
+    Msg Info "Will ignore the  directives in the MR description to increase version, if any."
     set VERSION 0
-  }
-  if {[lsearch $DESCRIPTION "*MINOR_VERSION*" ] >= 0} {
-    set VERSION 1
-  }
-  if {[lsearch $DESCRIPTION "*MAJOR_VERSION*" ] >= 0} {
-    set VERSION 2
-  } 
-  set merge_request_number $options(mr_id) 
-} else {
-  set VERSION 3
-}
-
-Msg Info "Version Level $VERSION"
-if {[catch {exec git merge --no-commit origin/$options(main_branch)} MRG]} {
-  Msg Error "Branch is outdated, please merge the latest changes from $options(main_branch) with:\n git fetch && git merge origin/$options(main_branch)\n"
-  exit 1	
-}
-
-Msg Info [exec $TclPath/CI/tag_repository.tcl -level $VERSION $onHOG $merge_request_number -default_version_level $options(default_version_level)]
-if {$options(push)!= ""} {
-  if {[catch {exec git push origin $options(push)} TMP]} {
-    Msg Warning $TMP
   } else {
-    Msg Info $TMP
-  }
-  if {[catch {exec git push --tags origin $options(push)} TMP]} {
-    Msg Warning $TMP
+    if {[lsearch $DESCRIPTION "*PATCH_VERSION*" ] >= 0} {
+      set VERSION 0
+    }
+    if {[lsearch $DESCRIPTION "*MINOR_VERSION*" ] >= 0} {
+      set VERSION 1
+    }
+    if {[lsearch $DESCRIPTION "*MAJOR_VERSION*" ] >= 0} {
+      set VERSION 2
+    } 
+    set merge_request_number $options(mr_id)
   } else {
-    Msg Info $TMP
+    set VERSION 3
   }
-}
+
+  Msg Info "Version Level $VERSION"
+  if {[catch {exec git merge --no-commit origin/$options(main_branch)} MRG]} {
+    Msg Error "Branch is outdated, please merge the latest changes from $options(main_branch) with:\n git fetch && git merge origin/$options(main_branch)\n"
+    exit 1	
+  }
+
+  Msg Info [exec $TclPath/CI/tag_repository.tcl -level $VERSION $onHOG $merge_request_number -default_version_level $options(default_version_level)]
+  if {$options(push)!= ""} {
+    if {[catch {exec git push origin $options(push)} TMP]} {
+      Msg Warning $TMP
+    } else {
+      Msg Info $TMP
+    }
+    if {[catch {exec git push --tags origin $options(push)} TMP]} {
+      Msg Warning $TMP
+    } else {
+      Msg Info $TMP
+    }
+  }

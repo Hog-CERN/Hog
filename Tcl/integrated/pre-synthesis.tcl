96ee37acb132e17ae5f9f1f3a2b8f9f5bf455da8
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-06-30 18:19:10 +0200

    WIP: typos

--- Tcl/integrated/pre-synthesis.tcl
+++ Tcl/integrated/pre-synthesis.tcl
@@ -60,8 +60,7 @@ if {$flavour != ""} {
 
 ############
 
-lassign [GetRepositoryVersions ./Top/$proj_name/$proj_name.tcl] commit version  hog_hash hog_ver  top_hash top_ver  libs hashes vers  subs subs_hashes  cons_ver cons_hash  ext_names ext_hashes  xml_hash xml_ver 
-
+lassign [GetRepositoryVersions [file normalize ./Top/$proj_name/$proj_name.tcl]] commit version  hog_hash hog_ver  top_hash top_ver  libs hashes vers  subs subs_hashes  cons_ver cons_hash  ext_names ext_hashes  xml_hash xml_ver 
 
 if {$xml_hash != 0} {
     set xml_dst $old_path/../xml

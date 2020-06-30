6fb3479b1d6a420c22922505c48c531c925d8832
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-06-30 20:43:14 +0200

    Cosmetics and comments

--- Tcl/integrated/pre-synthesis.tcl
+++ Tcl/integrated/pre-synthesis.tcl
@@ -58,8 +58,7 @@ if {$flavour != ""} {
   set flavour -1
 }
 
-############
-
+# Getting all the versions and SHAs of the repository
 lassign [GetRepoVersions ./Top/$proj_name/$proj_name.tcl] commit version  hog_hash hog_ver  top_hash top_ver  libs hashes vers  subs subs_hashes  cons_ver cons_hash  ext_names ext_hashes  xml_hash xml_ver 
 
 if {$xml_hash != 0} {
@@ -73,8 +72,6 @@ if {$xml_hash != 0} {
   set use_ipbus 0
 }
 
-
-############
 
 #number of threads
 set maxThreads [GetMaxThreads $proj_name]

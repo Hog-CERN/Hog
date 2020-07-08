0bd0890ce53a41a59943cc0dae6d8748f2ee198e
dcieri <davide.cieri@cern.ch>
2020-07-08 14:56:22 +0200

    debuggingg

--- Tcl/utils/generate_yaml.tcl
+++ Tcl/utils/generate_yaml.tcl
@@ -33,11 +33,11 @@ foreach dir [glob -type d $repo_path/Top/* ] {
     set ver [ GetProjectVersion $dir/$proj.tcl ]
 
     if {$ver != 0} {
-        puts $fp [ WriteYAMLStage "create_project" $proj ]
-        puts $fp [ WriteYAMLStage "simulate_project" $proj ]
-        puts $fp [ WriteYAMLStage "synthesise_ips" $proj ]
-        puts $fp [ WriteYAMLStage "syntesise_project" $proj ]
-        puts $fp [ WriteYAMLStage "implement_project" $proj ]
+        # puts $fp [ WriteYAMLStage "create_project" $proj ]
+        # puts $fp [ WriteYAMLStage "simulate_project" $proj ]
+        # puts $fp [ WriteYAMLStage "synthesise_ips" $proj ]
+        # puts $fp [ WriteYAMLStage "syntesise_project" $proj ]
+        # puts $fp [ WriteYAMLStage "implement_project" $proj ]
     }   
 }
 

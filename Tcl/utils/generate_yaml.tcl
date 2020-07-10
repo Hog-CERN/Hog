31133055dc61c7de52005e9473255b0735e1ff4a
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-07-10 16:15:58 +0100

    Try to fix gnerate_yaml.tcl

--- Tcl/utils/generate_yaml.tcl
+++ Tcl/utils/generate_yaml.tcl
@@ -15,49 +15,46 @@
 
 #parsing command options
 if {[catch {package require yaml} ERROR]} {
-  puts "$ERROR\n If you are running this script on tclsh, you can fix this by installing 'yaml'" 
+  puts "$ERROR\n If you are running this script on tclsh, you can fix this by installing 'tcllib'"
   return
 }
 set usage "- USAGE: $::argv0"
 set tcl_path [file normalize "[file dirname [info script]]/.."]
 
 set repo_path [pwd]
-cd $tcl_path
-source ./hog.tcl
+source $tcl_path/hog.tcl
 
 file copy -force $repo_path/Hog/hog-child.yml $repo_path/generated-config.yml
 set fp [open "$repo_path/generated-config.yml" a]
 puts $fp "\n"
 foreach dir [glob -type d $repo_path/Top/* ] {
-    set proj [ file tail $dir ]
-    set ver [ GetProjectVersion $dir/$proj.tcl ]
-    if {$ver == 0} {
-        if { [ file exists "$dir/ci.conf" ] == 1} {
-            set cifile [open $dir/ci.conf ]
-            set input [read $cifile]
-            set lines [split $input "\n"]
-            # Loop through each line
-            foreach line $lines {
-                # Do something with line here
-                set stage_and_prop [regexp -all -inline {\S+} $line]
-                set stage [lindex $stage_and_prop 0]
-                if {$stage != "" && ($stage == "create_project" || $stage == "simulate_project" || $stage == "synthesise_ips" || $stage == "synthesise_project" || $stage == "implement_project" )  } {
-                    puts $fp [ WriteYAMLStage $stage $proj ]
-                }
-            }
-        } else {
-            puts $fp [ WriteYAMLStage "create_project" $proj ]
-            puts $fp [ WriteYAMLStage "simulate_project" $proj ]
-            puts $fp [ WriteYAMLStage "synthesise_ips" $proj ]
-            puts $fp [ WriteYAMLStage "synthesise_project" $proj ]
-            puts $fp [ WriteYAMLStage "implement_project" $proj ]
-        }
-    }   
+  set proj [ file tail $dir ]
+  set ver [ GetProjectVersion $dir/$proj.tcl ]
+  if {$ver == 0} {
+    Msg Info "$proj was modified, adding it to CI..."
+    if { [ file exists "$dir/ci.conf" ] == 1} {
+      set cifile [open $dir/ci.conf ]
+      set input [read $cifile]
+      set lines [split $input "\n"]
+      # Loop through each line
+      foreach line $lines {
+	# Do something with line here
+	set stage_and_prop [regexp -all -inline {\S+} $line]
+	set stage [lindex $stage_and_prop 0]
+	if {$stage != "" && ($stage == "create_project" || $stage == "simulate_project" || $stage == "synthesise_ips" || $stage == "synthesise_project" || $stage == "implement_project" )  } {
+	  puts $fp [ WriteYAMLStage $stage $proj ]
+	}
+      }
+    } else {
+      puts $fp [ WriteYAMLStage "create_project" $proj ]
+      puts $fp [ WriteYAMLStage "simulate_project" $proj ]
+      puts $fp [ WriteYAMLStage "synthesise_ips" $proj ]
+      puts $fp [ WriteYAMLStage "synthesise_project" $proj ]
+      puts $fp [ WriteYAMLStage "implement_project" $proj ]
+    }
+  } else {
+    Msg Info "$proj was not modified since version: $ver, skipping."
+    #Here we should provide the link to the tag in $ver
+  }
 }
-
-# puts $fp "test:"
-# puts $fp "  stage: test"
-# puts $fp "  script:"
-# puts $fp "    - echo \"hello\""
-
 close $fp

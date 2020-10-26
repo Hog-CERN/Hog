06a01ecbe8a6cf8c30413cce8233f7fa653fe9bc
dcieri <davide.cieri@cern.ch>
2020-10-26 11:14:51 +0100

    debug echo

--- Other/GetArtifactsAndRename.sh
+++ Other/GetArtifactsAndRename.sh
@@ -30,6 +30,7 @@ else
     echo "Hog-INFO: downloading artifacts..."
     ref=refs/merge-requests%2F$mr%2Fhead
     curl --location --header "PRIVATE-TOKEN: ${push_token}" "$api"/projects/"${proj}"/jobs/artifacts/"$ref"/download?job="$job" -o output.zip
+    echo "curl --location --header "PRIVATE-TOKEN: ${push_token}" "$api"/projects/"${proj}"/jobs/artifacts/"$ref"/download?job="$job" -o output.zip"
 
     # GET all artifacts from user_post stage
 	pipeline=$(curl --globoff --header "PRIVATE-TOKEN: ${push_token}" "$api/projects/${proj}/merge_requests/$mr/pipelines" | jq '.[0].id')
@@ -37,6 +38,7 @@ else
     if [ "$job" != "$5" ]; 
     then
         curl --location --header "PRIVATE-TOKEN: ${push_token}" "$api"/projects/"${proj}"/jobs/artifacts/"$ref"/download?job="$job" -o output1.zip
+        echo "curl --location --header "PRIVATE-TOKEN: ${push_token}" "$api"/projects/"${proj}"/jobs/artifacts/"$ref"/download?job="$job" -o output1.zip"
         # echo "Hog-INFO: unzipping artifacts from $job job..."
         # unzip output1.zip -d bin1
         # mv -f bin1/bin/* bin/

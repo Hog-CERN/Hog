66aa36e29c84af431fafd593f163bf9c7b8633df
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-10-27 11:54:29 +0100

    typo

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -1780,9 +1780,9 @@ You can fix this by installing package \"tcllib\""
     Msg Info "Found the following yml files: $YML_FILES"
 
     set HOGYML_SHA [GetSHA $YML_FILES]
-    lassign [GitRet {log --format=%h -1 $YML_REF_F} $YML_FILES] ret EXPECTEDYML_SHA 
+    lassign [GitRet "log --format=%h -1 $YML_REF_F" $YML_FILES] ret EXPECTEDYML_SHA 
     if {$ret != 0} {
-      lassign [GitRet { log --format=%h -1 origin/$YML_REF_F} $YML_FILES] ret EXPECTEDYML_SHA 
+      lassign [GitRet "log --format=%h -1 origin/$YML_REF_F" $YML_FILES] ret EXPECTEDYML_SHA 
       if {$ret != 0} {
         Msg $MSG_TYPE "Error in project .gitlab-ci.yml. ref: $YML_REF not found"
         set EXPECTEDYML_SHA ""

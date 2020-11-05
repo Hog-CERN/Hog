4d78197709d0d7f025a02fb434d3cc53820cb5d6
dcieri <davide.cieri@cern.ch>
2020-11-05 10:35:05 +0100

    FEATURE: bugfix: restoring wildcards

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -447,15 +447,17 @@ proc ReadListFile {list_file path {lib ""} {sha_mode 0} } {
       set file_and_prop [regexp -all -inline {\S+} $line]
       set srcfile [lindex $file_and_prop 0]
       set srcfile "$path/$srcfile"
-      if {![file exists $srcfile]} {
-        Msg CriticalWarning "$srcfile not found in $path"
-        continue
-      }
+      
       set srcfiles [glob $srcfile]
       
       # glob the file list for wildcards
       if {$srcfiles != $srcfile} {
-	Msg Info "Wildcard source expanded from $srcfile to $srcfiles"
+	     Msg Info "Wildcard source expanded from $srcfile to $srcfiles"
+      } else {
+        if {![file exists $srcfile]} {
+          Msg CriticalWarning "$srcfile not found in $path"
+          continue
+        }
       }
       
       foreach vhdlfile $srcfiles {

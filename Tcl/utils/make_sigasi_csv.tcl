6e63b52d3dde78dd1377e63f245d0fe9aa87323d
Francesco Gonnella <francesco.gonnella@cern.ch>
2020-11-27 13:23:23 +0100

    FEATURE: create make_sigasi_csv.tcl script

--- Tcl/utils/make_sigasi_csv.tcl
+++ Tcl/utils/make_sigasi_csv.tcl
@@ -1,7 +1,66 @@
-#open_project -read_only -quiet project_1.xpr
-set source_files [get_files -filter {(FILE_TYPE == VHDL || FILE_TYPE == "VHDL 2008" || FILE_TYPE == VERILOG || FILE_TYPE == SYSTEMVERILOG) && USED_IN_SIMULATION == 1 } ]
-set csv_file [open "vivado_files.csv" w]
-foreach source_file $source_files {
-	puts  $csv_file [ concat  [ get_property LIBRARY $source_file ] "," $source_file ]
+#   Copyright 2018-2020 The University of Birmingham
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+# @file
+# Create sigasi csv file
+
+set path [file normalize "[file dirname [info script]]/.."]
+set old_path [pwd]
+source $path/hog.tcl
+
+if { $::argc != 1} {
+  Msg Info "Usage: make_sigasi_csv <project name>"
+  exit 1
+} 
+set project_name [lindex $argv 0]  
+set project_file [file normalize $path/../../Projects/$project_name/$project_name.xpr]
+
+if {[file exists $project_file]} {
+  Msg Info "Opening exixsting project file $project_file..."
+  open_project -quiet $project_file  
+} else {
+  Msg Info "Creating project: $project_name..." 
+  source $path/../../Top/$project_name/$project_name.tcl
 }
-#close_project
+
+#Simulation
+set csv_name "${project_name}_sigasi_sim.csv"
+#Create IPs here
+Msg Info "Generating IP targets for simulations..."
+foreach ip [get_ips] {
+  generate_target simulation $ip
+}
+
+Msg Info "Creating sigasi csv file for simulation $csv_name..."
+set source_files [get_files -filter {(FILE_TYPE == VHDL || FILE_TYPE == "VHDL 2008" || FILE_TYPE == VERILOG || FILE_TYPE == SYSTEMVERILOG) && USED_IN_SIMULATION == 1 } ]
+set csv_file [open $csv_name w]
+foreach source_file $source_files {
+  puts  $csv_file [ concat  [ get_property LIBRARY $source_file ] "," $source_file ]
+}
+close $csv_file
+
+#Synthesis
+set csv_name "${project_name}_sigasi_synth.csv"
+Msg Info "Generating IP targets for synthesis..."
+foreach ip [get_ips] {
+  generate_target synthesis $ip
+}
+
+Msg Info "Creating sigasi csv file for synthesis $csv_name..."
+set source_files [get_files -filter {(FILE_TYPE == VHDL || FILE_TYPE == "VHDL 2008" || FILE_TYPE == VERILOG || FILE_TYPE == SYSTEMVERILOG) && USED_IN_SYNTHESIS == 1 } ]
+set csv_file [open $csv_name w]
+foreach source_file $source_files {
+  puts  $csv_file [ concat  [ get_property LIBRARY $source_file ] "," $source_file ]
+}
+close $csv_file

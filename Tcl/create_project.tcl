41251211050cb1c5bb682cb771a8cd16e0265708
Francesco Gonnella <francesco.gonnella@cern.ch>
2021-05-24 15:04:52 +0100

    Remove ip_output_repo for PlanAhead

--- Tcl/create_project.tcl
+++ Tcl/create_project.tcl
@@ -90,8 +90,12 @@ proc CreateProject {} {
       set_param project.enableVHDL2008 1
       set_property "enable_vhdl_2008" 1 $obj
     }
-    set_property "ip_output_repo" $globalSettings::repo_path/IP $obj
 
+    if { [string first PlanAhead [version]] =! 0 } {
+      #This property doesn't exist in PlanAhead
+      set_property "ip_output_repo" $globalSettings::repo_path/IP $obj
+    }
+    
   } elseif {[info commands project_new] != ""} {
     package require ::quartus::project
     #QUARTUS_ONLY

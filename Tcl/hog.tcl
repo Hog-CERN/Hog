ba3615fa02a778d6c441d4a53f231b0c8f54178a
Davide Cieri <davide.cieri@cern.ch>
2021-08-12 11:36:51 +0200

    no 2008 for ISE

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -1819,8 +1819,6 @@ proc AddHogFiles { libraries properties {verbose 0}} {
               # ISE does not support vhdl2008
               if { [string first PlanAhead [version]] != 0 } {
                 set_property -name "file_type" -value "VHDL 2008" -objects $file_obj
-              } else {
-                Msg Warning "Xilinx PlanAhead/ISE does not support VHDL2008. Property not set for $f"
               }
             } else {
               Msg Info "Filetype is VHDL 93 for $f"

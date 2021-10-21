b973e8247627d4b512e3357d29138bfd3eecd8dc
Davide Cieri <davide.cieri@cern.ch>
2021-10-21 10:52:35 +0200

    FEATURE: Tracking all file types inside a .con

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -1676,7 +1676,7 @@ proc GetProjectFiles {} {
           }
         } elseif {[string equal $type "IP"]} {
           dict lappend libraries "IP" $f
-        } elseif {[string equal $type "XDC"]} {
+        } elseif {[string equal $fs_type "Constrs"]} {
           dict lappend libraries "XDC" $f
         } else {
           dict lappend libraries "OTHER" $f

be7684a6a8bf91da5d93a9242b114b4e2ddf2834
Davide Cieri <davide.cieri@cern.ch>
2022-03-02 10:06:17 +0100

    Bash script to upload badges

---
+++ Other/UploadBadges.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+file=$1
+current_badges=$(curl --header "PRIVATE-TOKEN: $HOG_PUSH_TOKEN" "$CI_API_V4_URL/projects/${CI_PROJECT_ID}/badges" --request GET)
+image_url=$(curl -s --request POST --header "PRIVATE-TOKEN: ${HOG_PUSH_TOKEN}" --form "file=@$file" ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/uploads | jq -r '.full_path')
+echo "Uploaded file $file to $image_url"
+badge_name=$(basename -s .svg $file)
+badge_exists=0
+for badge in $(echo "${current_badges}" | jq -r '.[] | @base64'); do
+  _jq() {
+    echo ${badge} | base64 --decode | jq -r ${1}
+  }
+  name=$(_jq '.name')
+  id=$(_jq '.id')
+  echo $name
+  echo $id
+  if [[ "$badge_name" == *"$name"* ]]; then
+    badge_exists=1
+    echo "Badge $badge_name exists, updating it..."
+    curl --header "PRIVATE-TOKEN: $HOG_PUSH_TOKEN" "$CI_API_V4_URL/projects/${CI_PROJECT_ID}/badges/1486" --request PUT --data "image_url=https://gitlab.cern.ch/${image_url}"
+  fi
+done
+
+if [[ $badge_exists == 0 ]]; then
+  echo "Badge $badge_name does not exist yet. Creating it..."
+  curl --header "PRIVATE-TOKEN: $HOG_PUSH_TOKEN" --request POST --data "link_url=https://gitlab.cern.ch/hog/test/TestFirmware/-/releases&image_url=https://gitlab.cern.ch/${image_url}&name=${badge_name}" "$CI_API_V4_URL/projects/${CI_PROJECT_ID}/badges"
+fi

e0c3ef446005c501531cde73e696ab5485827b84
Davide Cieri <davide.cieri@cern.ch>
2022-06-23 09:28:25 +0200

    Update post-implementation.tcl

--- Tcl/integrated/post-implementation.tcl
+++ Tcl/integrated/post-implementation.tcl
@@ -227,7 +227,7 @@ if {[IsXilinx]} {
 # Run user post-implementation file
 set user_post_implementation_file "./Top/$group_name/$proj_name/post-implementation.tcl"
 if {[file exists $user_post_implementation_file]} {
-  Msg Info "Sourcing user post-implementation file $user_post_bitstream_file"
+  Msg Info "Sourcing user post-implementation file $user_post_implementation_file"
   source $user_post_implementation_file
 }
 

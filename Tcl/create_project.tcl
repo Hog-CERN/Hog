65fef19d3d726757429756943a073ce2e333ac2d
Davide Cieri <davide.cieri@cern.ch>
2022-07-11 13:35:20 +0200

    removing debug printouts

--- Tcl/create_project.tcl
+++ Tcl/create_project.tcl
@@ -802,25 +802,25 @@ if {[IsQuartus]} {
     file delete -force -- "./hogTmp"
   }
 }
-puts "ciao"
+
 if {[file exists $post_file]} {
   Msg Info "Found post-creation Tcl script $post_file, executing it..."
   source $post_file
 }
 
 # Check extra IPs
-puts "ciao2"
+
 lassign [GetHogFiles -ext_path "$globalSettings::HOG_EXTERNAL_PATH" -repo_path $repo_path "$repo_path/Top/$DESIGN/list/"] listLibraries listProperties listMain
-puts "ciao3"
+
 CheckExtraFiles $listLibraries
-puts "ciao4"
+
 if {[IsXilinx]} {
   set old_path [pwd]
   cd $repo_path
   set flavour [GetProjectFlavour $DESIGN]
   # Getting all the versions and SHAs of the repository
   lassign [GetRepoVersions [file normalize $repo_path/Top/$DESIGN] $repo_path $globalSettings::HOG_EXTERNAL_PATH] commit version  hog_hash hog_ver  top_hash top_ver  libs hashes vers  cons_ver cons_hash  ext_names ext_hashes  xml_hash xml_ver user_ip_repos user_ip_hashes user_ip_vers
-  puts "ciao5"
+  
   set this_commit  [GetSHA]
 
   if {$commit == 0 } {

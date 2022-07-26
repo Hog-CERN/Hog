04d19c8ced7e9bd4ae858fba8d435bd1d969fde2
Davide Cieri <davide.cieri@cern.ch>
2022-07-26 11:58:45 +0200

    EOS_USER

--- Other/CheckEnv.sh
+++ Other/CheckEnv.sh
@@ -89,7 +89,7 @@ echo
 echo ===== ESSENTIAL VARIABLES =====
 echo -n "Variable: HOG_USER is "
 if [ -z ${HOG_USER+x} ]; then
-    echo "NOT defined. This variable is essential for git to work properly. It should be set to the username for your service account (a valid CERN account)."
+    echo "NOT defined. This variable is essential for git to work properly. It should be set to the username for your service account (a valid git account)."
     FAIL=1
 else
     echo "defined."
@@ -106,14 +106,23 @@ fi
 echo --------------------------------
 
 if ( ! ([ -z ${EOS_MGM_URL+x} ] && [ -z ${HOG_OFFICIAL_BIN_EOS_PATH+x} ])); then
-    echo -n "Variable: HOG_PASSWORD is "
-    if [ -z ${HOG_PASSWORD+x} ]; then
-        echo "NOT defined. This variable is essential for git to work properly. It should be set to your service's account password and masked."
+    echo -n "Variable: EOS_PASSWORD is "
+    if [ -z ${EOS_PASSWORD+x} ]; then
+        echo "NOT defined. This variable is essential to communicate with the CERN EOS cloud, to store IPs and official bitfiles."
         FAIL=1
     else
         echo "defined"
     fi
     echo --------------------------------
+    echo -n "Variable: EOS_USER is "
+    if [ -z ${EOS_USER+x} ]; then
+        echo "NOT defined. This variable is essential to communicate with the CERN EOS cloud, to store IPs and official bitfiles."
+        FAIL=1
+    else
+        echo "defined."
+    fi
+    echo --------------------------------
+
 fi
 
 echo -n "Variable: HOG_PUSH_TOKEN is "

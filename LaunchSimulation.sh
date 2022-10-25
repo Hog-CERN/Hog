e1bb72d794737624073a63002fe6c83849b49757
Davide Cieri <davide.cieri@cern.ch>
2022-10-25 15:31:39 +0200

    removing troublesome if

--- LaunchSimulation.sh
+++ LaunchSimulation.sh
@@ -198,13 +198,13 @@ function HogSimulateFunc(){
   SimulateProject $@
   # exit 0
 }
-if [[ ${BASH_SOURCE[0]} == $0 ]]; then
-#   printf "script '%s' is sourced in\n" "${BASH_SOURCE[0]}"
-# else
-  repoPath=$(dirname "$0")
-  print_hog $repoPath
-  SimulateProject $@
-fi
-# repoPath=$(dirname "$0")
-# print_hog $repoPath
-# create_project $@
+# if [[ ${BASH_SOURCE[0]} == $0 ]]; then
+# #   printf "script '%s' is sourced in\n" "${BASH_SOURCE[0]}"
+# # else
+#   repoPath=$(dirname "$0")
+#   print_hog $repoPath
+#   SimulateProject $@
+# fi
+repoPath=$(dirname "$0")
+print_hog $repoPath
+create_project $@

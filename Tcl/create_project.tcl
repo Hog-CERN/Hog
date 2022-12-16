2abfa9c7443146e5ac88355003dfe3cc2aa519db
Francesco Gonnella <francesco.gonnella@cern.ch>
2022-12-16 17:18:05 +0000

    Fix project created message

--- Tcl/create_project.tcl
+++ Tcl/create_project.tcl
@@ -957,4 +957,4 @@ if {[IsXilinx]} {
   cd $old_path
 }
 
-Msg Info "Project $DESIGN created successfully in globalSettings::build_dir."
+Msg Info "Project $DESIGN created successfully in $globalSettings::build_dir."

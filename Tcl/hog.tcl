baf66c94a345bfd3f8ae9a22bdbbc7a3468af4be
Francesco Gonnella <francesco.gonnella@cern.ch>
2023-02-17 14:59:54 +0000

    Change message in CHeckHogRelease

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -3882,7 +3882,7 @@ proc CheckLatestHogRelease {{repo_path .}} {
     Msg Status ""
     Msg Status "cd Hog && git checkout master && git pull"
     Msg Status ""
-    Msg Info "Also pdate the ref: in your .gitlab-ci.yml to $master_ver"
+    Msg Status "Also pdate the ref: in your .gitlab-ci.yml to $master_ver"
     Msg Status ""
   } else {
 

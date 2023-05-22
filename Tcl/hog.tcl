c36330e952f438bb9bcf653915bb913c74b0fa37
Francesco Gonnella <francesco.gonnella@cern.ch>
2023-05-22 10:56:20 +0100

    Create the GetLinkedFile functions to return linked files from a soft link with the correct path

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -686,12 +686,12 @@ proc ReadListFile args {
 
               set m [dict create]
               dict set m $lib$ext $main_lib$ext
-              dict lappend libraries $lib$ext $vhdlfile
+              dict lappend libraries $lib$ext [file normalize $vhdlfile]
 	      if {[file type $vhdlfile] eq "link"} {
 		#if the file is a link, also add the linked file
-		set real_file [file readlink $vhdlfile]
+		set real_file [GetLinkedFile $vhdlfile]
 		dict lappend libraries $lib$ext $real_file
-		Msg Info "File $vhdlfile is a soft link, also adding the real file: $real_file"
+		Msg Debug "File $vhdlfile is a soft link, also adding the real file: $real_file"
 	      }
 	      
               if {[dict exists $main_libs $lib$ext] == 0} {
@@ -710,15 +710,36 @@ proc ReadListFile args {
   if {$sha_mode != 0} {
     #In SHA mode we also need to add the list file to the list
     
-    dict lappend libraries $lib$ext $list_file
+    dict lappend libraries $lib$ext [file normalize $list_file]
     if {[file type $list_file] eq "link"} {
       #if the file is a link, also add the linked file
-      set real_file [file readlink $list_file]
+      set real_file [GetLinkedFile $list_file]
       dict lappend libraries $lib$ext $real_file
-      Msg Info "List file $list_file is a soft link, also adding the real file: $real_file"
+      Msg Debug "List file $list_file is a soft link, also adding the real file: $real_file"
     }    
   }
   return [list $libraries $properties $main_libs]
+}
+
+## @brief Return the real file linked by a soft link
+#
+# If the provided file is not a soft link, will give a Warning and return an empty string.
+# If the link is broken, will give a warning but still return the linked file
+#
+# @param[in] link_file the soft link file
+proc GetLinkedFile {link_file} {
+  if {[file type $link_file] eq "link"} {
+    set real_file [file normalize [file dirname $link_file]/[file link $link_file]]
+    if {![file exists $real_file]} {
+      Msg Warning "$link_file is a broken link, because the linked file: $real_file does not exist."
+    }
+    
+  } else {
+    Msg Warning "$link file is not a soft link"
+    set real_file ""
+  }
+
+  return $real_file
 }
 
 ## @brief Merge two tcl dictionaries of lists
@@ -1259,7 +1280,8 @@ proc GetRepoVersions {proj_dir repo_path {ext_path ""} {sim 0}} {
     #library names have a .src extension in values returned by GetHogFiles
     set name [file rootname [file tail $f]]
     lassign [GetVer  $files] ver hash
-    #Msg Info "Found source list file $f, version: $ver commit SHA: $hash"
+    Msg Debug "Found source list file $f, version: $ver commit SHA: $hash"
+    Msg Debug "Files in list: $files"
     lappend libs $name
     lappend versions $ver
     lappend vers $ver

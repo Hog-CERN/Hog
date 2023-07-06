bf2449f2da65a9e90e2afd7faad1e543cc1e0b77
Francesco Gonnella <francesco.gonnella@cern.ch>
2023-07-06 16:01:15 +0200

    CreateProject place holder launches Hog L in case of 0 arguments

--- CreateProject.sh
+++ CreateProject.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
- OLD_DIR=`pwd`
- DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
- cd "${DIR}/.."
- echo "Hog [Warning]: $0 is obsolete, you should use ./Hog/Hog now!"
- ./Hog/Hog CREATE $@
- cd "${OLD_DIR}"
+OLD_DIR=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${DIR}/.."
+echo "Hog [Warning]: $0 is obsolete, you should use ./Hog/Hog now!"
+
+if [ "$#" -eq 0 ]; then
+    ./Hog/Hog LIST
+else
+    ./Hog/Hog CREATE $@
+fi
+
+cd "${OLD_DIR}"

54118a876587fe6a6873f27ac5dad7765c1c8de8
Davide Cieri <davide.cieri@cern.ch>
2023-07-17 15:08:37 +0200

    removing dirtiness

--- LaunchSimulation.sh
+++ LaunchSimulation.sh
@@ -3,5 +3,5 @@
  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
  cd "${DIR}/.."
  echo "Hog [Warning]: $0 is obsolete, you should use ./Hog/Do now!"
- ./Hog/Do SIMULATE "$@" "${!#}"
+ ./Hog/Do SIMULATE "$@"
  cd "${OLD_DIR}"

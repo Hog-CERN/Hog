aa9dcf0529cb26538cb6bf45aa3c65f1a5dbc458
Davide Cieri <davide.cieri@cern.ch>
2024-03-04 10:25:48 +0100

    commenting setting of PYTHONHOME env

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -1611,7 +1611,7 @@ proc ExtractVersionFromTag {tag} {
 # @param[in] generate    if set to 1, tells the function to generate the VHDL decode address files rather than check them
 #
 proc CopyXMLsFromListFile {list_file path dst {xml_version "0.0.0"} {xml_sha "00000000"}  {generate 0} } {
-  set ::env(PYTHONHOME) "/usr"
+  # set ::env(PYTHONHOME) "/usr"
   lassign  [ExecuteRet python -c "from __future__ import print_function; from sys import path;print(':'.join(path\[1:\]))"] ret msg
   if {$ret == 0} {
     set ::env(PYTHONPATH) $msg

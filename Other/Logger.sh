e4b6a4df0e824c051d46184c9dcae6f602b3c5c7
Guillermo Loustau <guillermo.jose.loustau.de.linares@cern.ch>
2024-08-05 21:07:23 +0200

    trying to get tcl pid and experimente the proper pid to kill

--- Other/Logger.sh
+++ Other/Logger.sh
@@ -30,13 +30,19 @@ export ENABLE_MSG_TYPE_CNT=1
 HOG_LOG_EN=0
 HOG_COLOR_EN=0
 export clrschselected="dark"
-export fail_when_error=0
-error_failing=0
 
-export error_delay=0
-export error_fail=0
+export ENABLE_FWE=0
+export hog_sh_pid=""
+export hog_pid=""
 export error_pid=""
+export tcl_pid=""
+export launch_tcl_pid=""
+export fail_when_error=0
+export fwe_failing=0
+export fwe_delay=0
+export error_fail=0
 export failing_en=0
+export fwe_fail_trig=0
 
 # export
 
@@ -373,24 +379,124 @@ function log_stdout(){
           echo "${msgHeadBW[$msgType]} ${dataLine#${msgRemove[$msgType]}} "  >> $LOG_INFO_FILE;
         fi
       fi
-      if [[ "$msgType" == "error" ]]; then
-        if (( $fail_when_error > 0 )); then
-          error_failing=$fail_when_error
-          failing_en=1
-          error_pid=$BASHPID
-          Msg Debug "Process $error_pid will be killed in $error_failing"
-        fi
-      fi
-      if [[ $failing_en -gt 0 ]];then
-        if [[ $error_failing -gt 1 ]]; then
-          error_failing=$(($error_failing - 1))
-          # Msg Debug "Process $BASHPID will be killed in $error_failing"
+      # if [[ "$msgType" == "error" ]]; then
+      #   if (( $fail_when_error > 0 )); then
+      #     fwe_failing=$fail_when_error
+      #     failing_en=1
+      #     error_pid=$BASHPID
+      #     Msg Debug "Process $error_pid will be killed in $fwe_failing"
+      #   fi
+      # fi
+      # if [[ $failing_en -gt 0 ]];then
+      #   if [[ $fwe_failing -gt 1 ]]; then
+      #     fwe_failing=$(($fwe_failing - 1))
+      #     # Msg Debug "Process $BASHPID will be killed in $fwe_failing"
+      #   else
+      #     Msg Error "exitaaaando"
+      #     kill -9 "$error_pid"
+      #     exit 2
+      #   fi
+      # fi
+      # if [[ $failing_en -gt -1 ]];then
+      # export ENABLE_FWE=0
+      # export hog_pid=""
+      # export error_pid=""
+      # export fail_when_error=0
+      # export fwe_failing=0
+      # export fwe_delay=0
+      # export fwe_fail_trig=0
+      # export failing_en=0
+
+      if [[ $ENABLE_FWE -eq 1 ]];then
+        # Msg Debug "$fwe_fail_trig "
+        if [[ $fwe_fail_trig -eq 0 ]]; then
+          if [[ "$msgType" == "error" ]]; then
+            # if (( $fail_when_error > 0 )); then
+              fwe_failing=$fwe_delay
+              fwe_fail_trig=1
+              error_fail=1
+              error_pid=$BASHPID
+              Msg Warning "Process $error_pid will be killed in $fwe_failing"
+            # fi
+          fi
         else
-          Msg Error "exitaaaando"
-          kill -9 "$error_pid"
-          exit 2
+          if [[ $failing_en -eq 0 ]];then
+            if [[ $fwe_failing -gt 0 ]];then
+              fwe_failing=$(($fwe_failing - 1))
+              # Msg Debug "Process $BASHPID will be killed in $fwe_failing"
+              Msg Debug "Process $error_pid will be killed in $fwe_failing"
+              Msg Debug "Hog_PID = $hog_pid"
+              Msg Debug "hog_sh_pid = $hog_sh_pid"
+              Msg Debug "error_pid = $error_pid"
+              Msg Debug "tcl_pid = $tcl_pid"
+
+              
+
+
+
+
+
+
+
+
+
+
+              Msg Debug "launch_tcl_pid = $launch_tcl_pid"
+            else
+
+
+              # last_pids=$(ps -eo pid,cmd --sort=pid | tail -n 6)
+              # # tcl_cmd_pid=$($last_pids | grep "tcl" ) #| awk '{print $1}')
+              # echo "Last 3 PIDs:"
+              # echo "$last_pids"
+              # # echo "TCL PID : $tcl_cmd_pid"
+              # # Define the substring to search for
+              # search_string="launch.tcl"
+
+              # # Initialize a variable to hold the matching line
+              # matching_line=""
+
+              # # Read the multiline string line by line
+              # while IFS= read -r line; do
+              #   echo "$line"
+              #   if [[ "$line" == *"$search_string"* ]]; then
+              #     matching_line="$line"
+              #     break  # Exit the loop after finding the first match
+              #   fi
+              # done <<< "$last_pids"
+
+              # # Check if a matching line was found
+              # if [[ -n "$matching_line" ]]; then
+              #   echo "Found matching line: $matching_line"
+              # else
+              #   echo "No matching line found."
+              # fi
+              # if [[ $matching_line =~ ([0-9]{2,}) ]]; then
+              #   first_multidigit_number="${BASH_REMATCH[1]}"
+              #   echo "The first multidigit number is: $first_multidigit_number"
+              # else
+              #   echo "No multidigit number found."
+              # fi
+              # launch_tcl_pid="$first_multidigit_number"
+
+
+
+
+
+              Msg Error "exitaaaando"
+              # kill -SIGINT "$error_pid"
+              # kill -SIGINT "-$hog_pid"
+              # kill -SIGINT "-$launch_tcl_pid"
+              # pkill -P "$launch_tcl_pid"
+              wait "$launch_tcl_pid" 2>/dev/null
+              echo "Process $hog_pid has been terminated."
+              failing_en=-1
+              Hog_exit
+            fi
+          fi
         fi
       fi
+      
     done
   fi
 }
@@ -409,11 +515,16 @@ function Hog_exit () {
   echo "======================================== "
   if [[ $(msg_counter r e) -gt 0 ]]; then
     echo -e "$txtred *** Hog finished with errors *** $txtwht"
+              kill -SIGINT "-$hog_pid"
+
     exit 1
   else
     echo -e "$txtgrn *** Hog finished  without errors *** $txtwht"
+              kill -SIGINT "-$hog_pid"
+
     exit 0
   fi
+
 }
 
 ## @function Log_capture()
@@ -428,6 +539,43 @@ function Log_capture(){
   # $* > >(test1 "stdout") 2> >(test2 "stderr") &
   tcl_pid=$!
   Msg Debug "pid = $tcl_pid"
+  # last_pids=$(ps -eo pid,cmd --sort=pid | tail -n 6)
+  # # tcl_cmd_pid=$($last_pids | grep "tcl" ) #| awk '{print $1}')
+  # echo "Last 3 PIDs:"
+  # echo "$last_pids"
+  # # echo "TCL PID : $tcl_cmd_pid"
+  # # Define the substring to search for
+  # search_string="launch.tcl"
+
+  # # Initialize a variable to hold the matching line
+  # matching_line=""
+
+  # # Read the multiline string line by line
+  # while IFS= read -r line; do
+  #   echo "$line"
+  #   if [[ "$line" == *"$search_string"* ]]; then
+  #     matching_line="$line"
+  #     break  # Exit the loop after finding the first match
+  #   fi
+  # done <<< "$last_pids"
+
+  # # Check if a matching line was found
+  # if [[ -n "$matching_line" ]]; then
+  #   echo "Found matching line: $matching_line"
+  # else
+  #   echo "No matching line found."
+  # fi
+  # if [[ $matching_line =~ ([0-9]{2,}) ]]; then
+  #   first_multidigit_number="${BASH_REMATCH[1]}"
+  #   echo "The first multidigit number is: $first_multidigit_number"
+  # else
+  #   echo "No multidigit number found."
+  # fi
+  # launch_tcl_pid="$first_multidigit_number"
+
+  # echo "TCL PID : $launch_tcl_pid"
+
+  
   while kill -0 $tcl_pid 2>/dev/null; do
     sleep 1
   done
@@ -538,6 +686,27 @@ function Msg() {
       # echo "${msgHeadBW[$msgType]} ${dataLine#${msgRemove[$msgType]}} "  >> $LOG_INFO_FILE;
     fi
   fi
+  # if [[ $failing_en -gt 0 ]];then
+  #   if [[ $fwe_failing -gt 1 ]]; then
+  #     fwe_failing=$(($fwe_failing - 1))
+  #     # Msg Debug "Process $BASHPID will be killed in $fwe_failing"
+  #     Msg Debug "Process $error_pid will be killed in $fwe_failing"
+  #   else
+  #     Msg Error "exitaaaando"
+  #     kill -9 "$error_pid"
+  #     failing_en=0
+  #     exit 2
+  #   fi
+  # else
+  #   if [[ "$msgType" == "error" ]]; then
+  #     if (( $fail_when_error > 0 )); then
+  #       fwe_failing=$fail_when_error
+  #       failing_en=1
+  #       error_pid=$BASHPID
+  #       Msg Warning "Process $error_pid will be killed in $fwe_failing"
+  #     fi
+  #   fi
+  # fi
   return 0
 }
 
@@ -661,10 +830,12 @@ process_HogEnv_config() {
   #
   # @param[in] execution line to process
 function Logger_Init() {
+  hog_sh_pid=$BASHPID
   DEBUG_VERBOSE=4
   if [[ "$*" =~ "-verbose" ]]; then
     DEBUG_VERBOSE=5
   fi
+  hog_pid=$BASHPID
 
   ROOT_PROJECT_FOLDER=$(pwd)
   LOG_INFO_FILE=$ROOT_PROJECT_FOLDER"/hog_info.log"
@@ -822,23 +993,25 @@ function Logger_Init() {
   if [[ -v Hog_Prj_dict["fail_when_error.enabled"] ]]; then
     if [[ ${Hog_Prj_dict["fail_when_error.enabled"]} =~ ^[01]$ ]]; then
       if [[ -v Hog_Prj_dict["fail_when_error.delay"] ]]; then
+        ENABLE_FWE=1
         Msg Warning "Fail when error enabled"
         if [[ ${Hog_Prj_dict["fail_when_error.delay"]} =~ ^[0-9]+$ ]]; then
-          fail_when_error=$((1 + ${Hog_Prj_dict["fail_when_error.delay"]}))
+          fwe_delay=$((${Hog_Prj_dict["fail_when_error.delay"]}))
           Msg Warning "The fail delay is set to $fail_when_error"
         else
-          fail_when_error=10
+          
+          fwe_delay=10
           Msg Warning "The variable fail_when_error.delay is not only nunmbers delay set to 10"
         fi
       fi
     else
       Msg Warning "The variable terminal.logger is not 1 or 0, Default to 0"
-      fail_when_error=0
+      fwe_delay=0
     fi
   else
-    fail_when_error=0
+    fwe_delay=0
   fi
-  Msg Debug "fail_when_error = $fail_when_error"
+  Msg Debug "fail_when_error delay = $fwe_delay"
   # hog_user_fwe="${current_user}_fail_when_error_enabled"
   # hog_user_dfwe="${current_user}_fail_when_error_delay"
   # if [[ -v Hog_Prj_dict["$hog_user_fwe"] ]]; then

4d9b000dbe5533c7d3008d2a8885f3b475c225ba
Davide Cieri <davide.cieri@cern.ch>
2024-11-25 13:10:40 +0100

    Max length of project name with font 11.0 is 20 characters

--- Tcl/CI/create_badges.tcl
+++ Tcl/CI/create_badges.tcl
@@ -19,10 +19,14 @@
 
 proc generate_prj_badge {prj_name ver color file} {
     set font_size 11.0
-    set max_characters 31.0
+    set max_characters 20.0
+    puts [string length $prj_name]
     if { [expr {[string length $prj_name] > $max_characters}] } {
       set scaling_factor [expr { $max_characters / [string length $prj_name] } ]
+      puts $font_size
       set font_size [expr {ceil($scaling_factor*$font_size)}]
+      puts $scaling_factor
+      puts $font_size
     }
 
     set svg_content "<?xml version=\"1.0\" encoding=\"UTF-8\"?>

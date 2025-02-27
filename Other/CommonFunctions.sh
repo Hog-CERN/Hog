7191a5594ef722d3189683077759172f7567b0b1
Davide Cieri <davide.cieri@cern.ch>
2025-02-27 11:55:53 +0100

    Removed Logger dependency from Common Functions

--- Other/CommonFunctions.sh
+++ Other/CommonFunctions.sh
@@ -77,7 +77,7 @@ export DEBUG_VERBOSE=""
 #
 function select_command_from_line() {
   if [ -z ${1+x} ]; then
-    Msg Error " missing input! Got: $1!"
+    echo "[ERROR] Missing input! Got: $1!"
     return 1
   fi
 
@@ -85,45 +85,45 @@ function select_command_from_line() {
 
   if [[ $TCL_FIRST_LINE =~ 'vivado' ]]; then
     if [[ $TCL_FIRST_LINE =~ 'vivadoHLS' ]]; then
-      Msg Info " Recognised VivadoHLS project"
+      echo "[INFO] Recognised VivadoHLS project"
       COMMAND="vivado_hls"
       COMMAND_OPT="-f"
     else
-      Msg Info " Recognised Vivado project"
+      echo "[INFO] Recognised Vivado project"
       COMMAND="vivado"
       COMMAND_OPT="-nojournal -nolog -mode batch -notrace -source "
       POST_COMMAND_OPT="-tclargs "
     fi
   elif [[ $TCL_FIRST_LINE =~ 'quartus' ]]; then
     if [[ $TCL_FIRST_LINE =~ 'quartusHLS' ]]; then
-      Msg Error " Intel HLS compiler is not supported!"
+      echo "[ERROR] Intel HLS compiler is not supported!"
       return 1
     else
-      Msg Info " Recognised QuartusPrime project"
+      echo "[INFO] Recognised QuartusPrime project"
       COMMAND="quartus_sh"
       COMMAND_OPT="-t "
     fi
   elif [[ $TCL_FIRST_LINE =~ 'intelHLS' ]]; then
-    Msg Error "Intel HLS compiler is not supported!"
+    echo "[ERROR] Intel HLS compiler is not supported!"
     return 1
   elif [[ $TCL_FIRST_LINE =~ 'planahead' ]]; then
-    Msg Info " Recognised planAhead project"
+    echo "[INFO] Recognised planAhead project"
     COMMAND="planAhead"
     COMMAND_OPT="-nojournal -nolog -mode batch -notrace -source "
     POST_COMMAND_OPT="-tclargs"
   elif [[ $TCL_FIRST_LINE =~ 'libero' ]]; then
-    Msg Info "Recognised Libero SoC project"
+    echo "[INFO] Recognised Libero SoC project"
     COMMAND="libero"
     COMMAND_OPT="SCRIPT:"
     POST_COMMAND_OPT="SCRIPT_ARGS:"
   elif [[ $TCL_FIRST_LINE =~ 'diamond' ]]; then
-    Msg Info "Recognised Lattice Diamond project"
+    echo "[INFO] Recognised Lattice Diamond project"
     COMMAND="diamondc"
     COMMAND_OPT=" "
     POST_COMMAND_OPT=" "
   else
-    Msg Warning " You should write #vivado, #quartus or #planahead as first line in your hog.conf file or project Tcl file, assuming Vivado... "
-    Msg Info " Recognised Vivado project"
+    echo "[WARNING] You should write #vivado, #quartus or #planahead as first line in your hog.conf file or project Tcl file, assuming Vivado... "
+    echo "[INFO] Recognised Vivado project"
     COMMAND="vivado"
     COMMAND_OPT="-mode batch -notrace -source"
     POST_COMMAND_OPT="-tclargs"
@@ -159,13 +159,13 @@ function select_command() {
     file="$tcl"
     FILE_TYPE=TCL
   else
-    Msg Error "No suitable file found in $1!"
+    echo "[ERROR] No suitable file found in $1!"
     return 1
   fi
 
 
   if ! select_command_from_line "$(head -1 "$file")"; then
-    Msg Error "Failed to select COMMAND, COMMAND_OPT and POST_COMMAND_OPT"
+    echo "[ERROR] Failed to select COMMAND, COMMAND_OPT and POST_COMMAND_OPT"
     return 1
   fi
 
@@ -190,7 +190,7 @@ function select_command() {
 #
 function select_compiler_executable() {
   if [ "a$1" == "a" ]; then
-    Msg Error "select_compiler_executable(): Variable COMMAND is not set!"
+    echo "[ERROR] select_compiler_executable(): Variable COMMAND is not set!"
     return 1
   fi
 
@@ -199,23 +199,23 @@ function select_compiler_executable() {
   else
     if [ "$1" == "vivado" ]; then
       if [ -z ${XILINX_VIVADO+x} ]; then
-        Msg Error "No vivado executable found and no variable XILINX_VIVADO set\n"
+        echo "[ERROR] No vivado executable found and no variable XILINX_VIVADO set\n"
         cd "${OLD_DIR}" || exit
         return 1
       elif [ -d "$XILINX_VIVADO" ]; then
-        Msg Info "XILINX_VIVADO is set to '$ XILINX_VIVADO'"
+        echo "[INFO] XILINX_VIVADO is set to '$ XILINX_VIVADO'"
         HDL_COMPILER="$XILINX_VIVADO/bin/$1"
       else
-        Msg Error "Failed locate '$1' executable from XILINX_VIVADO: $XILINX_VIVADO"
+        echo "[ERROR] Failed locate '$1' executable from XILINX_VIVADO: $XILINX_VIVADO"
         return 1
       fi
     elif [ "$1" == "quartus_sh" ]; then
       if [ -z ${QUARTUS_ROOTDIR+x} ]; then
-        Msg Error "No quartus_sh executable found and no variable QUARTUS_ROOTDIR set\n"
+        echo "[ERROR] No quartus_sh executable found and no variable QUARTUS_ROOTDIR set\n"
         cd "${OLD_DIR}" || exit
         return 1
       else
-        Msg Info "QUARTUS_ROOTDIR is set to '$QUARTUS_ROOTDIR'"
+        echo "[INFO] QUARTUS_ROOTDIR is set to '$QUARTUS_ROOTDIR'"
         #Decide if you are to use bin or bin 64
         #Note things like $PROCESSOR_ARCHITECTURE==x86 won't work in Windows because this will return the version of the git bash
         if [ -d "$QUARTUS_ROOTDIR/bin64" ]; then
@@ -223,16 +223,16 @@ function select_compiler_executable() {
         elif [ -d "$QUARTUS_ROOTDIR/bin" ]; then
           HDL_COMPILER="$QUARTUS_ROOTDIR/bin/$1"
         else
-          Msg Error "Failed locate '$1' executable from QUARTUS_ROOTDIR: $QUARTUS_ROOTDIR"
+          echo "[ERROR] Failed locate '$1' executable from QUARTUS_ROOTDIR: $QUARTUS_ROOTDIR"
           return 1
         fi
       fi
     elif [ "$1" == "libero" ]; then
-      Msg Error "No libero executable found."
+      echo "[ERROR] No libero executable found."
       cd "${OLD_DIR}" || exit
       return 1
     else
-      Msg Error "cannot find the executable for $1."
+      echo "[ERROR] cannot find the executable for $1."
       echo "Probable causes are:"
       echo "- $1 was not setup"
       echo "- command not available on the machine"
@@ -257,19 +257,19 @@ function select_compiler_executable() {
 #
 function select_executable_from_project_dir() {
   if [ -z ${1+x} ]; then
-    Msg Error "missing input! Got: $1!"
+    echo "[ERROR] missing input! Got: $1!"
     return 1
   fi
 
   if ! select_command "$1"; then
-    Msg Error "Failed to select project type: exiting!"
+    echo "[ERROR] Failed to select project type: exiting!"
     return 1
   fi
 
   #select full path to executable and place it in HDL_COMPILER global variable
 
   if ! select_compiler_executable $COMMAND; then
-    Msg Error "Failed to get HDL compiler executable for $COMMAND"
+    echo "[ERROR] Failed to get HDL compiler executable for $COMMAND"
     return 1
   fi
 
@@ -282,7 +282,7 @@ function select_executable_from_project_dir() {
 # @brief prints the hog logo
 function print_hog() {
   if [ -z ${1+x} ]; then
-    Msg Error "missing input! Got: $1!"
+    echo "[ERROR] missing input! Got: $1!"
     return 1
   fi
   cd "$1" || exit
@@ -304,7 +304,7 @@ function print_hog() {
 # @brief prints the hog logo
 function print_log_hog() {
   if [ -z ${1+x} ]; then
-    Msg Error "Missing input! Got: $1!"
+    echo "[ERROR] Missing input! Got: $1!"
     return 1
   fi
   cat ${ROOT_PROJECT_FOLDER}"/Hog/images/hog_logo.txt"
@@ -319,7 +319,7 @@ function print_log_hog() {
 # @brief prints the hog logo
 function new_print_hog() {
   if [ -z ${1+x} ]; then
-    Msg Error "missing input! Got: $1!"
+    echo "[ERROR] missing input! Got: $1!"
     return 1
   fi
   cd "$1"
@@ -344,7 +344,7 @@ function new_print_hog() {
 #
 function search_projects() {
   if [ -z ${1+x} ]; then
-    Msg Error "missing input! Got: $1!"
+    echo "[ERROR] missing input! Got: $1!"
     return 1
   fi
 
@@ -352,7 +352,7 @@ function search_projects() {
     for dir in "$1"/*; do
       if [ -f "$dir/hog.conf" ]; then
         subname=${dir#*Top/}
-        Msg Info $subname
+        echo $subname
       else
         search_projects "$dir"
       fi
@@ -368,14 +368,14 @@ function search_projects() {
 # @returns  0 if success, 1 if failure
 #
 function HogVer() {
-  Msg Info "Checking the latest available Hog version..."
+  echo "Checking the latest available Hog version..."
   if ! check_command timeout
   then
     return 1
   fi
 
   if [ -z ${1+x} ]; then
-    Msg Error "Missing input! You should give the path to your Hog submodule. Got: $1!"
+    echo "ERROR: Missing input! You should give the path to your Hog submodule. Got: $1!"
     return 1
   fi
 
@@ -390,16 +390,16 @@ function HogVer() {
 
     # The next line checks if master_sha is an ancestor of current_sha
     if [ "$merge_base" != "$master_sha" ]; then
-      Msg Info
-      Msg Info "Version $master_version has been released (https://gitlab.cern.ch/hog/Hog/-/releases/$master_version)"
-      Msg Info "You should consider updating Hog submodule with the following instructions:"
       echo
-      Msg Info "cd Hog && git checkout master && git pull"
+      echo "Version $master_version has been released (https://gitlab.cern.ch/hog/Hog/-/releases/$master_version)"
+      echo "You should consider updating Hog submodule with the following instructions:"
       echo
-      Msg Info "Remember also to update the ref: in your .gitlab-ci.yml to $master_version"
+      echo "cd Hog && git checkout master && git pull"
+      echo
+      echo "Remember also to update the ref: in your .gitlab-ci.yml to $master_version"
       echo
     else
-      Msg Info "Latest official version is $master_version, nothing to do."
+      echo "Latest official version is $master_version, nothing to do."
     fi
 
   fi
@@ -416,7 +416,7 @@ function HogVer() {
 function check_command() {
   if ! command -v "$1" &> /dev/null
   then
-    Msg Warning "Command $1 could not be found"
+    echo "WARNING: Command $1 could not be found"
     return 1
   fi
   return 0

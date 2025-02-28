b6640d27c2afb432b6464492ed55bf117299eba4
Davide Cieri <davide.cieri@cern.ch>
2025-02-28 12:18:31 +0100

    sourcing HogPrint.sh instead of CommonFunctions.sh in before_script

---
+++ Other/HogPrint.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+#   Copyright 2018-2025 The University of Birmingham
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+
+# @fn print_hog
+#
+# @param[in] $1 path to Hog dir
+# @brief prints the hog logo
+function print_hog() {
+  if [ -z ${1+x} ]; then
+    echo "Error: missing input! Got: $1!"
+    return 1
+  fi
+  cd "$1" || exit
+  ver=$(git describe --always)
+  HOG_GIT_VERSION=$(git describe --always)
+  echo
+  cat ./images/hog_logo.txt
+  echo " Version: ${ver}"
+  echo
+  cd "${OLDPWD}" || exit >> /dev/null
+  HogVer "$1"
+
+  return 0
+}
+
+function HogVer() {
+  echo "Info: Checking the latest available Hog version..."
+  if ! check_command timeout
+  then
+    return 1
+  fi
+
+  if [ -z ${1+x} ]; then
+    echo "Error: Missing input! You should give the path to your Hog submodule. Got: $1!"
+    return 1
+  fi
+
+  if [[ -d "$1" ]]; then
+    cd "$1" || exit
+    current_version=$(git describe --always)
+    current_sha=$(git log "$current_version" -1 --format=format:%H)
+    timeout 5s git fetch
+    master_version=$(git describe origin/master)
+    master_sha=$(git log "$master_version" -1 --format=format:%H)
+    merge_base=$(git merge-base "$current_sha" "$master_sha")
+
+    # The next line checks if master_sha is an ancestor of current_sha
+    if [ "$merge_base" != "$master_sha" ]; then
+      echo
+      echo "Info: Version $master_version has been released (https://gitlab.cern.ch/hog/Hog/-/releases/$master_version)"
+      echo "Info: You should consider updating Hog submodule with the following instructions:"
+      echo
+      echo "Info: cd Hog && git checkout master && git pull"
+      echo
+      echo "Info: Remember also to update the ref: in your .gitlab-ci.yml to $master_version"
+      echo
+    else
+      echo "Info: Latest official version is $master_version, nothing to do."
+    fi
+
+  fi
+  cd ${OLDPWD} || exit >> /dev/null
+}

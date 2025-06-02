9a187b912e682b19ad881fc983e7c4289e1ca8a1
Joeal Subash <joeal.subash@cern.ch>
2025-06-02 13:17:39 +0100

    remove supress_newline; found better solution

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -2030,8 +2030,6 @@ proc GetHogFiles args {
   set ext_path $options(ext_path)
   set print_log $options(print_log)
 
-  set supress_newline_opt ""
-
   if { $sha_mode == 1 } {
     set sha_mode_opt "-sha_mode"
   } else {
@@ -2056,9 +2054,9 @@ proc GetHogFiles args {
   foreach f $list_files {
     set ext [file extension $f]
     if {$ext == ".ext"} {
-      lassign [ReadListFile {*}"$sha_mode_opt $print_log_opt $supress_newline_opt $f $ext_path"] l p fs
+      lassign [ReadListFile {*}"$sha_mode_opt $print_log_opt $f $ext_path"] l p fs
     } else {
-      lassign [ReadListFile {*}"$sha_mode_opt $print_log_opt $supress_newline_opt $f $repo_path"] l p fs
+      lassign [ReadListFile {*}"$sha_mode_opt $print_log_opt $f $repo_path"] l p fs
     }
     set libraries [MergeDict $l $libraries]
     set properties [MergeDict $p $properties]
@@ -5114,7 +5112,6 @@ proc ReadListFile {args} {
     {fileset.arg "" "The name of the library, from the main list file"}
     {sha_mode "If set, the list files will be added as well and the IPs will be added to the file rather than to the special IP library. The SHA mode should be used when you use the lists to calculate the git SHA, rather than to add the files to the project."}
     {print_log "If set will print stuff."}
-    {supress_newline "Will supress newline when called recursively for printing lisfile log"}
   }
   set usage "USAGE: ReadListFile \[options\] <list file> <path>"
   if {[catch {array set options [cmdline::getoptions args $parameters $usage]}] ||  [llength $args] != 2 } {
@@ -5127,7 +5124,6 @@ proc ReadListFile {args} {
   set lib $options(lib)
   set fileset $options(fileset)
   set print_log $options(print_log)
-  set supress_newline $options(supress_newline)
 
   if { $sha_mode == 1} {
     set sha_mode_opt "-sha_mode"
@@ -5140,13 +5136,6 @@ proc ReadListFile {args} {
   } else {
     set print_log_opt ""
   }
-
-  if { $supress_newline == 1 } {
-    set supress_newline_opt "-supress_newline"
-  } else {
-    set supress_newline_opt ""
-  }
-
 
   # if no library is given, work it out from the file name
   if {$lib eq ""} {
@@ -5178,11 +5167,10 @@ proc ReadListFile {args} {
   set data [split $file_data "\n"]
   set n [llength $data]
   if {$print_log == 1} {
-    if {$supress_newline == 0} {puts ""}
     dict set list_tree_dict $list_file "$data"
     Msg Info "Reading list file $list_file. Its contents are :"
     PrintDictItems $list_tree_dict
-    }
+  }
   Msg Debug "$n lines read from $list_file."
   set cnt 0
 
@@ -5228,10 +5216,10 @@ proc ReadListFile {args} {
               set ref_path [file normalize $path/$ref_path]
             }
 	    if {$print_log == 1} {
-	      Msg Info "List file $vhdlfile found in list file, Recursively opening it."
+	      Msg Info "List file $vhdlfile found in list file. Recursively opening it."
 	    }
             Msg Debug "List file $vhdlfile found in list file, recursively opening it using path \"$ref_path\"..."
-            lassign [ReadListFile {*}"-lib $library -fileset $fileset $sha_mode_opt $print_log_opt $supress_newline_opt $vhdlfile $ref_path"] l p fs
+            lassign [ReadListFile {*}"-lib $library -fileset $fileset $sha_mode_opt $print_log_opt $vhdlfile $ref_path"] l p fs
             set libraries [MergeDict $l $libraries]
             set properties [MergeDict $p $properties]
             set filesets [MergeDict $fs $filesets]
@@ -6135,18 +6123,20 @@ proc WriteUtilizationSummary {input output project_name run} {
 }
 
 proc PrintDictItems { {dct} {msg ""} {rmExt 0} } {
+  set ext_list {.src .sim .ext .ipb .con .sim}
   foreach item [dict keys $dct] {
     set val [dict get $dct $item]
-    # puts ""
     if { "$rmExt" == 1 } {
       set item [file rootname "$item"]
     }
-    Msg Info "$msg $item :"
       foreach lstitm $val {
 	  if {![regexp {^ *$} $lstitm] & ![regexp {^ *\#} $lstitm] } {
+	    set tl [file extension [lindex "$lstitm" 0]]
+	    set lsr [lsearch $ext_list $tl]
 	    puts "$lstitm"
 	  }
-	}
+      }
+      if {"$lsr" < 0} {puts ""}
       }
 }
 proc dict'sort {dict args} {

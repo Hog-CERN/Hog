8623d060042fe37962b2a3d7b670516d4c7a8af2
Davide Cieri <davide.cieri@cern.ch>
2025-08-21 16:55:42 +0200

    fix toml parse

--- Tcl/hog.tcl
+++ Tcl/hog.tcl
@@ -3851,7 +3851,7 @@ proc InitLauncher {script tcl_path parameters commands argv {custom_commands ""}
   }
 
   # Check if HogEnv.conf exists and parse it
-  if {![file exists [Hog::LoggerLib::GetUserFilePath "HogEnv.conf"]] } {
+  if {[file exists [Hog::LoggerLib::GetUserFilePath "HogEnv.conf"]] } {
     Msg Debug "HogEnv.conf not found"
     set loggerdict [Hog::LoggerLib::ParseTOML [Hog::LoggerLib::GetUserFilePath "HogEnv.conf" ]]
     set HogEnvDict [Hog::LoggerLib::GetTOMLDict]

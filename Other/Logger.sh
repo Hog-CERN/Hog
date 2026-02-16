946a4996178d27e42da0f7607ee4e69cab1f29d2
aranzaba <nordin.aranzabal@esrf.fr>
2026-02-16 21:43:40 +0100

    Other/Logger.sh: fix double EE in ERROR messages (EERROR -> ERROR)

--- Other/Logger.sh
+++ Other/Logger.sh
@@ -412,7 +412,11 @@ function log_stdout(){
         elif [[ $HOG_COLOR_EN -gt 0 ]]; then
           echo -e "${stderr_ack}${simpleColor[$msgType]} $dataLine $txtwht"
         else
-          echo -e "${stderr_ack}$dataLine"
+          if [[ "${stderr_ack}" == "E" && "${msgType}" == "error" ]]; then
+            echo -e " $dataLine"
+          else
+            echo -e "${stderr_ack}$dataLine"
+          fi
         fi
       # else
         # msg_counter w ${msgCounter[$msgType]} >> /dev/null

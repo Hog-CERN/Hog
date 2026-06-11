c36b9c5f04d247bb07ad783bde219505f29d34c0
Davide Cieri <davide.cieri@cern.ch>
2026-06-11 10:25:11 +0200

    feat: show star prompt after logo in Logger path

--- Other/Logger.sh
+++ Other/Logger.sh
@@ -823,6 +823,7 @@ function print_hog_logo () {
         echo -e "$line"
       fi
     done < "$logo_file"
+    echo "  ★  Like Hog? Star us on GitLab: https://gitlab.com/hog-cern/Hog  |  GitHub: https://github.com/hog-cern/Hog"
     export HOG_LOGO_PRINTED=1
   else
     Msg Warning "Logo file $logo_file doesn't exist"

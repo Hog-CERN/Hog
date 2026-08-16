6b5ec098637b53a8ad85421801e91b1452ddfee9
aranzaba <nordin.aranzabal@esrf.fr>
2026-08-16 18:31:31 +0200

    vitis flow: surface child process errors and keep vivado logs in vitis flows

--- Tcl/launch.tcl
+++ Tcl/launch.tcl
@@ -172,7 +172,7 @@ if {$NO_DIRECTIVE_FOUND == 1} {
         set ret [catch {exec -ignorestderr {*}$cmd >@ stdout 2>@ stderr} result]
 
         if {$ret != 0} {
-          Msg Error "IDE returned an error state."
+          Msg Error "IDE returned an error state: $result"
         } else {
           Msg Info "All done."
           exit 0
@@ -665,7 +665,7 @@ if {$cmd == -1} {
   set ret [catch {exec -ignorestderr {*}$cmd >@ stdout 2>@ stderr} result]
 
   if {$ret != 0} {
-    Msg Error "IDE returned an error state."
+    Msg Error "IDE returned an error state: $result"
   } else {
     Msg Info "All done."
     exit 0
